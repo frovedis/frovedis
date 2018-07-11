54d69a0f410225882a65e4c2239c1aafc58880be
Takuya Araki <t-araki@dc.jp.nec.com>
2018-07-11 20:58:16 +0900

    changed to create out directory if it does not exist

--- src/foreign_if/spark/examples/run_demo.sh
+++ src/foreign_if/spark/examples/run_demo.sh
@@ -10,6 +10,9 @@ COMMAND="mpirun -np 2 ../../server/frovedis_server"
 # by default, Spark runs as local mode
 # if you use distributed mode, put the input files to HDFS
 # hadoop fs -put input
+if [ ! -d out ]; then
+	mkdir out
+fi
 spark-submit --driver-java-options "-Djava.library.path=$LIBRARY_PATH" --jars $JARS --conf spark.driver.memory=8g lib/spark_frovedis_data_transfer.jar "$COMMAND"
 spark-submit --driver-java-options "-Djava.library.path=$LIBRARY_PATH" --jars $JARS --conf spark.driver.memory=8g lib/spark2frovedis_lrsgd.jar "$COMMAND"
 spark-submit --driver-java-options "-Djava.library.path=$LIBRARY_PATH" --jars $JARS --conf spark.driver.memory=8g lib/spark2frovedis_lrm.jar  "$COMMAND"
