b52a24d004ff48e42e1d4fba3cd179e47c15eb45
Takuya Araki <t-araki@dc.jp.nec.com>
2018-09-29 19:29:17 +0900

    added checking define of USE_CEREAL_FOR_EXRPC

--- src/foreign_if/exrpc/inst_archive.cc
+++ src/foreign_if/exrpc/inst_archive.cc
@@ -1,6 +1,9 @@
+#include "frovedis/core/config.hpp"
+#ifndef USE_CEREAL_FOR_EXRPC
 #include <boost/integer.hpp>
 #include "portable_iarchive.hpp"
 #include "portable_oarchive.hpp"
 
 // just instanciate archive
 // see the comment of above headers
+#endif
