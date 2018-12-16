c88cd167fc47610621ca4155492b2b0b3c7371bc
Takuya Araki <t-araki@dc.jp.nec.com>
2018-12-16 16:48:55 +0900

    changed not to use loop raking when size is small

--- src/frovedis/core/prefix_sum.hpp
+++ src/frovedis/core/prefix_sum.hpp
@@ -5,22 +5,15 @@
 #include "utility.hpp"
 
 #define PREFIX_SUM_VLEN 256
+#define PREFIX_SUM_VLEN_MIN 32 // minimum VLEN of 2nd part
 
 namespace frovedis {
 
 #if defined(_SX) || defined(__ve__)
 template <class T>
 void prefix_sum(const T* valp, T* outp, size_t size) {
-  if(size == 0) return;
-  size_t each = size / PREFIX_SUM_VLEN;
-  if(each % 2 == 0 && each > 1) each--;
-  size_t rest = size - each * PREFIX_SUM_VLEN;
-  T current_val[PREFIX_SUM_VLEN];
-#pragma _NEC vreg(current_val)
-  for(int i = 0; i < PREFIX_SUM_VLEN; i++) {
-    current_val[i] = 0;
-  }
-  if(each == 0) {
+  // If vector length for second part is small, do scalar
+  if (size < PREFIX_SUM_VLEN * PREFIX_SUM_VLEN_MIN) {
     T current_val = 0;
     // no vector loop
     for(size_t j = 0; j < size; j++) {
@@ -29,34 +22,44 @@ void prefix_sum(const T* valp, T* outp, size_t size) {
       outp[j] = next_val;
       current_val = next_val;
     }
-  } else {
-    for(size_t j = 0; j < each; j++) {
-      for(size_t i = 0; i < PREFIX_SUM_VLEN; i++) {
-        auto loaded_v = valp[j + each * i];
-        auto next_val = loaded_v + current_val[i];
-        outp[j + each * i] = next_val;
-        current_val[i] = next_val;
-      }
+    return;
+  }
+  // do vectorized version of prefix_sum
+  size_t each = size / PREFIX_SUM_VLEN;
+  if(each % 2 == 0 && each > 1) each--;
+  size_t rest = size - each * PREFIX_SUM_VLEN;
+  T current_val[PREFIX_SUM_VLEN];
+#pragma _NEC vreg(current_val)
+  for(int i = 0; i < PREFIX_SUM_VLEN; i++) {
+    current_val[i] = 0;
+  }
+  // each should always be >= 1
+  for(size_t j = 0; j < each; j++) {
+    for(size_t i = 0; i < PREFIX_SUM_VLEN; i++) {
+      auto loaded_v = valp[j + each * i];
+      auto next_val = loaded_v + current_val[i];
+      outp[j + each * i] = next_val;
+      current_val[i] = next_val;
     }
-    size_t rest_idx_start = each * PREFIX_SUM_VLEN;
-    T current_val_rest = 0;
-    for(size_t j = 0; j < rest; j++) {
-      auto loaded_v = valp[j + rest_idx_start];
-      auto next_val = loaded_v + current_val_rest;
-      outp[j + rest_idx_start] = next_val;
-      current_val_rest = next_val;
-    }
-    for(size_t i = 1; i < PREFIX_SUM_VLEN; i++) {
-      T to_add = outp[each * i - 1];
-      for(size_t j = each * i; j < each * (i+1); j++) {
-        outp[j] += to_add;
-      }
-    }
-    T to_add = outp[each * PREFIX_SUM_VLEN - 1];
-    for(size_t j = each * PREFIX_SUM_VLEN;
-        j < each * PREFIX_SUM_VLEN + rest; j++) {
+  }
+  size_t rest_idx_start = each * PREFIX_SUM_VLEN;
+  T current_val_rest = 0;
+  for(size_t j = 0; j < rest; j++) {
+    auto loaded_v = valp[j + rest_idx_start];
+    auto next_val = loaded_v + current_val_rest;
+    outp[j + rest_idx_start] = next_val;
+    current_val_rest = next_val;
+  }
+  for(size_t i = 1; i < PREFIX_SUM_VLEN; i++) {
+    T to_add = outp[each * i - 1];
+    for(size_t j = each * i; j < each * (i+1); j++) {
       outp[j] += to_add;
     }
+  }
+  T to_add = outp[each * PREFIX_SUM_VLEN - 1];
+  for(size_t j = each * PREFIX_SUM_VLEN;
+      j < each * PREFIX_SUM_VLEN + rest; j++) {
+    outp[j] += to_add;
   }
   return;
 }
