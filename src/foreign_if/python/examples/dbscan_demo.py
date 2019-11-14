023df9696e0f1233ec23f0d2d3cda41d30c3cd4e
Takuya Araki <t-araki@dc.jp.nec.com>
2019-11-14 17:20:53 +0900

    added dbscan demo (NTI)

---
+++ src/foreign_if/python/examples/dbscan_demo.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from frovedis.exrpc.server import FrovedisServer
+from frovedis.mllib.cluster import DBSCAN
+import sys
+import numpy as np
+
+# initializing the Frovedis server
+argvs = sys.argv
+argc = len(argvs)
+if (argc < 2):
+    print ('Please give frovedis_server calling command as the first argument \n(e.g. "mpirun -np 2 -x /opt/nec/nosupport/frovedis/ve/bin/frovedis_server")')
+    quit()
+FrovedisServer.initialize(argvs[1])
+
+# creating dbscan object
+dbm = DBSCAN(eps=3, metric='euclidean', verbose=0)
+
+# fitting the training matrix on dbscan object
+tmat = np.loadtxt("./input/knn_data.txt")
+tmat = np.asarray(tmat, dtype=np.float64)
+dbm.fit(tmat)
+
+lbl = dbm.labels_
+print ("fit_predict: ", dbm.fit_predict(tmat))
+print ("Labels: ", lbl)
+
+dbm.release()
+FrovedisServer.shut_down()
