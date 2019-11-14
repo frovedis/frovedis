26289f5c2dfd76b30a97ac3da160755abd524ef0
Takuya Araki <t-araki@dc.jp.nec.com>
2019-11-14 17:36:23 +0900

    fixed normalize issue, modified return type of load_graph to return object with loaded graph metadata, added knn model utilities in server (NTI)

--- src/foreign_if/server/exrpc_util_headers.hpp
+++ src/foreign_if/server/exrpc_util_headers.hpp
@@ -4,8 +4,10 @@
 #include "../exrpc/exrpc_expose.hpp"
 #include "frovedis_mem_pair.hpp"
 #include "dummy_matrix.hpp"
+#include "dummy_graph.hpp"
 #include "dummy_model.hpp"
 #include "pca_result.hpp"
+#include "knn_result.hpp"
 #include "scalapack_result.hpp"
 #include "model_tracker.hpp"
 
