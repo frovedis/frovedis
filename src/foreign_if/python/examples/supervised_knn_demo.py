e4c11a9a288ee35aa7716ddc27fc9426c99da11a
Takuya Araki <t-araki@dc.jp.nec.com>
2019-11-14 17:20:29 +0900

    added knn supervised and unsupervised demo for python wrapper (NTI)

---
+++ src/foreign_if/python/examples/supervised_knn_demo.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import sys
+import numpy as np
+from frovedis.exrpc.server import FrovedisServer
+from frovedis.mllib.neighbors import KNeighborsClassifier
+from frovedis.mllib.neighbors import KNeighborsRegressor
+
+# initializing the Frovedis server
+argvs = sys.argv
+argc = len(argvs)
+if (argc < 2):
+    print ('Please give frovedis_server calling command as the first argument \n(e.g. "mpirun -np 2 -x /opt/nec/nosupport/frovedis/ve/bin/frovedis_server")')
+    quit()
+FrovedisServer.initialize(argvs[1])
+
+samples = np.loadtxt("./input/knn_data.txt", dtype=np.float64)
+lbl = [10, 10, 10, 20, 10, 20]
+
+# fitting input matrix and label on KNeighbors Classifier object
+knc = KNeighborsClassifier(n_neighbors=3, algorithm='brute', metric='euclidean')
+knc.fit(samples, lbl)
+
+dist, ind = knc.kneighbors(samples)
+print(dist)
+print(ind)
+
+graph = knc.kneighbors_graph(samples, mode="distance")
+print(graph)
+
+print("predicting on classifier model: ")
+print (knc.predict(samples))
+print("prediction probability on classifier model: ")
+proba = knc.predict_proba(samples)
+print(proba)
+print("Accuracy: ")
+print(knc.score(samples, lbl))
+
+# fitting input matrix and label on KNeighbors Regressor object
+lbl2 = [1.2,0.3,1.1,1.9,1.7,0.5]
+knr = KNeighborsRegressor(n_neighbors=3, algorithm='brute', metric='euclidean')
+knr.fit(samples, lbl2)
+
+#dist, ind = knr.kneighbors(samples)
+#print(dist)
+#print(ind)
+
+#graph = knr.kneighbors_graph(samples, mode="distance")
+#print(graph)
+
+print("predicting on regressor model: ")
+print (knr.predict(samples))
+print("RMSE: ")
+print(knr.score(samples, lbl2))
+
+# clean-up
+knc.release()
+knr.release()
+FrovedisServer.shut_down()
---
+++ src/foreign_if/python/examples/unsupervised_knn_demo.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import sys
+import numpy as np
+from frovedis.mllib.neighbors import NearestNeighbors
+from frovedis.exrpc.server import FrovedisServer
+
+# initializing the Frovedis server
+argvs = sys.argv
+argc = len(argvs)
+if (argc < 2):
+    print ('Please give frovedis_server calling command as the first argument \n(e.g. "mpirun -np 2 -x /opt/nec/nosupport/frovedis/ve/bin/frovedis_server")')
+    quit()
+FrovedisServer.initialize(argvs[1])
+
+samples = np.loadtxt("./input/knn_data.txt", dtype=np.float64)
+knn = NearestNeighbors(n_neighbors=3, radius=2.0, 
+                         algorithm='brute', metric='euclidean')
+knn.fit(samples)
+
+distances, indices = knn.kneighbors()
+print(distances)
+print(indices)
+
+graph = knn.kneighbors_graph(samples)
+print(graph)
+
+dist, ind = knn.radius_neighbors()
+for i in range(len(dist)):
+  print(dist[i])
+  print(ind[i])
+
+rad_graph = knn.radius_neighbors_graph()
+print(rad_graph)
+
+FrovedisServer.shut_down()
