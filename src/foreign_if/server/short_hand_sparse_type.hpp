f965b8af7fe687d423d14d925f9943a77962d8e1
Takuya Araki <t-araki@dc.jp.nec.com>
2019-11-14 17:38:52 +0900

    fixed knn server side template related issue (NTI)

--- src/foreign_if/server/short_hand_sparse_type.hpp
+++ src/foreign_if/server/short_hand_sparse_type.hpp
@@ -9,21 +9,21 @@ namespace frovedis {
   typedef crs_matrix<DT2> S_MAT2;
   typedef crs_matrix_local<DT1> S_LMAT1;
   typedef crs_matrix_local<DT2> S_LMAT2;
-  typedef crs_matrix<DT1,DT4> S_MAT14;
-  typedef crs_matrix<DT1,DT5> S_MAT15;
-  typedef crs_matrix<DT2,DT4> S_MAT24;
-  typedef crs_matrix<DT2,DT5> S_MAT25;
-  typedef crs_matrix<DT3,DT4> S_MAT34;
-  typedef crs_matrix<DT3,DT5> S_MAT35;
-  typedef crs_matrix<DT4,DT4> S_MAT44;
-  typedef crs_matrix<DT4,DT5> S_MAT45;
-  typedef crs_matrix_local<DT1,DT4> S_LMAT14;
-  typedef crs_matrix_local<DT1,DT5> S_LMAT15;
-  typedef crs_matrix_local<DT2,DT4> S_LMAT24;
-  typedef crs_matrix_local<DT2,DT5> S_LMAT25;
-  typedef crs_matrix_local<DT3,DT4> S_LMAT34;
-  typedef crs_matrix_local<DT3,DT5> S_LMAT35;
-  typedef crs_matrix_local<DT4,DT4> S_LMAT44;
-  typedef crs_matrix_local<DT4,DT5> S_LMAT45;
+  typedef crs_matrix<DT1,DT4,DT5> S_MAT14;
+  typedef crs_matrix<DT1,DT5,DT5> S_MAT15;
+  typedef crs_matrix<DT2,DT4,DT5> S_MAT24;
+  typedef crs_matrix<DT2,DT5,DT5> S_MAT25;
+  typedef crs_matrix<DT3,DT4,DT5> S_MAT34;
+  typedef crs_matrix<DT3,DT5,DT5> S_MAT35;
+  typedef crs_matrix<DT4,DT4,DT5> S_MAT44;
+  typedef crs_matrix<DT4,DT5,DT5> S_MAT45;
+  typedef crs_matrix_local<DT1,DT4,DT5> S_LMAT14;
+  typedef crs_matrix_local<DT1,DT5,DT5> S_LMAT15;
+  typedef crs_matrix_local<DT2,DT4,DT5> S_LMAT24;
+  typedef crs_matrix_local<DT2,DT5,DT5> S_LMAT25;
+  typedef crs_matrix_local<DT3,DT4,DT5> S_LMAT34;
+  typedef crs_matrix_local<DT3,DT5,DT5> S_LMAT35;
+  typedef crs_matrix_local<DT4,DT4,DT5> S_LMAT44;
+  typedef crs_matrix_local<DT4,DT5,DT5> S_LMAT45;
 }
 #endif
