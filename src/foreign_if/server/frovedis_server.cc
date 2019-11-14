28f74a6c80be4b5922eb013cc2fffe754e638835
Takuya Araki <t-araki@dc.jp.nec.com>
2019-11-14 17:36:51 +0900

    added server code for basic graph operations and page rank (NTI)

--- src/foreign_if/server/frovedis_server.cc
+++ src/foreign_if/server/frovedis_server.cc
@@ -10,6 +10,7 @@ void expose_frovedis_dataframe_functions();
 void expose_frovedis_wrapper_functions();
 void expose_frovedis_ml_functions();
 void expose_frovedis_model_functions();
+void expose_frovedis_graph_functions();
 
 int main(int argc, char* argv[]) {
   frovedis::use_frovedis use(argc, argv);
@@ -22,6 +23,7 @@ int main(int argc, char* argv[]) {
   expose_frovedis_wrapper_functions();
   expose_frovedis_ml_functions();
   expose_frovedis_model_functions();
+  expose_frovedis_graph_functions();
   frovedis::init_frovedis_server(argc, argv);
   return 0;
 }
