0b9bea9b23d0dc7e4565173348b553f8d68285cc
Takuya Araki <t-araki@dc.jp.nec.com>
2020-02-10 17:10:19 +0900

    exposed missing calls for show/release glm data of dense type (NTI)

--- src/foreign_if/server/expose_mldata.cc
+++ src/foreign_if/server/expose_mldata.cc
@@ -9,9 +9,11 @@ void expose_frovedis_ml_data_functions() {
   expose((load_local_glm_data<DT1,S_LMAT1>));
   expose((create_and_set_glm_data<DT1,S_MAT1,S_LMAT1>));
   expose((release_glm_data<DT1,S_MAT1>));
-  expose((release_glm_data<DT1,D_MAT1>));
+  expose((release_glm_data<DT1,R_MAT1>));
+  expose((release_glm_data<DT1,C_MAT1>));
   expose((show_glm_data<DT1,S_MAT1>));
-  expose((show_glm_data<DT1,D_MAT1>));
+  expose((show_glm_data<DT1,R_MAT1>));
+  expose((show_glm_data<DT1,C_MAT1>));
   // --- frovedis generic data ---
   expose(load_local_data<std::vector<std::string>>);
   expose((release_data<lvec<int>>));           //ScaLAPACK(IPIV)
