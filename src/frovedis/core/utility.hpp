96f0c0b30e78ac62459d8e85e1246da69c2f1a10
Takuya Araki <t-araki@dc.jp.nec.com>
2020-03-23 16:13:10 +0900

    add save[load]binary_local

--- src/frovedis/core/utility.hpp
+++ src/frovedis/core/utility.hpp
@@ -4,6 +4,11 @@
 #include "rlog.hpp"
 #include <cmath>
 #include <limits>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
 
 #define SIZEOF_INT8 1
 #define SIZEOF_INT16 2
@@ -94,6 +99,63 @@ bool confirm_given_type_against_expected(const std::string& e_tname,
     throw std::runtime_error(errmsg);
   }
   else return true;
+}
+
+template <class T>
+void savebinary_local(const T* v, size_t size, const std::string& path) {
+  int fd = ::open(path.c_str(), O_CREAT|O_WRONLY|O_TRUNC, 0666);
+  if(fd == -1) {
+    throw std::runtime_error("open failed: " + std::string(strerror(errno)));
+  }
+  size_t to_write = size * sizeof(T);
+  const char* buf = reinterpret_cast<const char*>(v);
+  while(to_write > 0) {
+    auto written = ::write(fd, buf, to_write);
+    if(written == -1) {
+      ::close(fd);
+      throw std::runtime_error("write failed: " + std::string(strerror(errno)));
+    } else {
+      to_write -= written;
+      buf += written;
+    }
+  }
+  ::close(fd);
+}
+
+template <class T>
+void savebinary_local(const std::vector<T>& v, const std::string& path) {
+  savebinary_local(v.data(), v.size(), path);
+}
+
+template <class T>
+std::vector<T> loadbinary_local(const std::string& path) {
+  int fd = ::open(path.c_str(), O_RDONLY);
+  if(fd == -1) {
+    throw std::runtime_error("open failed: " + std::string(strerror(errno)));
+  }
+  struct stat sb;
+  if(stat(path.c_str(), &sb) != 0) {
+    ::close(fd);
+    throw std::runtime_error("stat failed: " + std::string(strerror(errno)));
+  }
+  auto to_read = sb.st_size;
+  auto size = to_read / sizeof(T);
+  if(size * sizeof(T) != to_read) 
+    throw std::runtime_error("file size is not multiple of data size");
+  std::vector<T> ret(size);
+  char* buf = reinterpret_cast<char*>(ret.data());
+  while(to_read > 0) {
+    auto read_size = ::read(fd, buf, to_read);
+    if(read_size == -1) {
+      ::close(fd);
+      throw std::runtime_error("read failed: " + std::string(strerror(errno)));
+    } else {
+      to_read -= read_size;
+      buf += read_size;
+    }
+  }
+  ::close(fd);
+  return ret;
 }
 
 // temporary; to improve vectorization
