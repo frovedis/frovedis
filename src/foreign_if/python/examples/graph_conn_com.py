56ccfb9446cc49066f30b56335e44b749f8134d2
Takuya Araki <t-araki@dc.jp.nec.com>
2020-03-25 14:05:02 +0900

    modified SSSP and ConnectedComponent demo as per latest code (NTI)

--- src/foreign_if/python/examples/graph_conn_com.py
+++ src/foreign_if/python/examples/graph_conn_com.py
@@ -2,6 +2,7 @@ import sys
 import numpy as np
 from frovedis.exrpc.server import *
 import frovedis.networkx as fnx
+import networkx as nx
 
 # initializing the Frovedis server
 argvs = sys.argv
@@ -10,12 +11,15 @@ if argc < 2:
     print ('Please give frovedis_server calling command as the first argument \n(e.g. "mpirun -np 2 -x /opt/nec/nosupport/frovedis/ve/bin/frovedis_server")')
     quit()
 
+fname = 'input/cit-Patents_10.txt'
 FrovedisServer.initialize(argvs[1])
-frov_graph = fnx.read_edgelist('input/cit-Patents_10.txt')
+frov_graph = fnx.read_edgelist(fname, nodetype=np.int64)
 frov_graph.debug_print()
 
-fnx.connected_components(frov_graph) # it prints result on terminal
-
+#fnx.connected_components(frov_graph, print_summary=True)
+ret = fnx.connected_components(frov_graph)
+print(ret)
 frov_graph.release()
+
 # Shutting down the Frovedis server
 FrovedisServer.shut_down()
--- src/foreign_if/python/examples/graph_sssp.py
+++ src/foreign_if/python/examples/graph_sssp.py
@@ -2,6 +2,7 @@ import sys
 import numpy as np
 from frovedis.exrpc.server import *
 import frovedis.networkx as fnx
+import networkx as nx
 
 # initializing the Frovedis server
 argvs = sys.argv
@@ -10,13 +11,24 @@ if (argc < 2):
     print ('Please give frovedis_server calling command as the first argument \n(e.g. "mpirun -np 2 -x /opt/nec/nosupport/frovedis/ve/bin/frovedis_server")')
     quit()
 
+source = 1
+
+fname = 'input/cit-Patents_10.txt' 
+
 FrovedisServer.initialize(argvs[1])
-frov_graph = fnx.read_edgelist('input/cit-Patents_10.txt')
+#frov_graph = fnx.read_edgelist(fname, nodetype=np.int32)
+frov_graph = fnx.read_edgelist(fname, nodetype=np.int32, create_using=nx.DiGraph())
 frov_graph.debug_print()
 
 #returns dict of lists containing shortest path from source to all other nodes
-source = 4
 ret = fnx.single_source_shortest_path(frov_graph, source) 
+print("Frovedis res: ")
+print(ret)
+
+#nx_graph = nx.read_edgelist(fname, nodetype=np.int32)
+nx_graph = nx.read_edgelist(fname, nodetype=np.int32, create_using=nx.DiGraph())
+ret = nx.single_source_shortest_path(nx_graph, source) 
+print("Networkx res: ")
 print(ret)
 
 frov_graph.release()
