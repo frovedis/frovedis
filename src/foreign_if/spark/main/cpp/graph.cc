3f54f6180fba5bd938bb678234c48a9ff0d75645
Takuya Araki <t-araki@dc.jp.nec.com>
2020-07-04 18:53:14 +0900

    modified spark graphx wrapper code as per new chnages in library code (NTI)

--- src/foreign_if/spark/main/cpp/graph.cc
+++ src/foreign_if/spark/main/cpp/graph.cc
@@ -12,7 +12,7 @@ JNIEXPORT void JNICALL Java_com_nec_frovedis_Jexrpc_JNISupport_showGraph
   auto fm_node = java_node_to_frovedis_node(env, master_node);
   auto f_dptr = static_cast<exrpc_ptr_t> (fdata);
   try {
-    exrpc_oneway(fm_node, show_graph<graph>, f_dptr);
+    exrpc_oneway(fm_node, show_graph<graph<DT1>>, f_dptr);
   }
   catch(std::exception& e) { set_status(true,e.what()); }
 }
@@ -22,7 +22,7 @@ JNIEXPORT void JNICALL Java_com_nec_frovedis_Jexrpc_JNISupport_releaseGraph
   auto fm_node = java_node_to_frovedis_node(env, master_node);
   auto f_dptr = static_cast<exrpc_ptr_t> (fdata);
   try {
-    exrpc_oneway(fm_node, release_graph<graph>, f_dptr);
+    exrpc_oneway(fm_node, release_graph<graph<DT1>>, f_dptr);
   }
   catch(std::exception& e) { set_status(true,e.what()); }
 }
@@ -33,7 +33,7 @@ JNIEXPORT jobject JNICALL Java_com_nec_frovedis_Jexrpc_JNISupport_loadGraphFromT
   auto fm_node = java_node_to_frovedis_node(env, master_node);
   dummy_graph ret; 
   try {
-    ret = exrpc_async(fm_node, load_graph<graph>, c_fname).get();
+    ret = exrpc_async(fm_node, (load_graph<graph<DT1>,DT1>), c_fname).get();
   }
   catch(std::exception& e) { set_status(true,e.what()); }
   return to_jDummyGraph(env, ret);
@@ -46,7 +46,7 @@ JNIEXPORT void JNICALL Java_com_nec_frovedis_Jexrpc_JNISupport_saveGraph
   auto fm_node = java_node_to_frovedis_node(env, master_node);
   auto f_dptr = static_cast<exrpc_ptr_t> (fdata);
   try {
-    exrpc_oneway(fm_node, save_graph<graph>, f_dptr, c_fname);
+    exrpc_oneway(fm_node, save_graph<graph<DT1>>, f_dptr, c_fname);
   }
   catch(std::exception& e) { set_status(true,e.what()); }
 }
@@ -57,7 +57,7 @@ JNIEXPORT jlong JNICALL Java_com_nec_frovedis_Jexrpc_JNISupport_copyGraph
   auto f_dptr = static_cast<exrpc_ptr_t> (fdata);
   exrpc_ptr_t gptr = -1;
   try {
-    gptr = exrpc_async(fm_node, copy_graph<graph>, f_dptr).get();
+    gptr = exrpc_async(fm_node, copy_graph<graph<DT1>>, f_dptr).get();
   }
   catch(std::exception& e) { set_status(true,e.what()); }
   return (long) gptr;
@@ -69,76 +69,90 @@ JNIEXPORT jlong JNICALL Java_com_nec_frovedis_Jexrpc_JNISupport_setGraphData
   auto f_dptr = static_cast<exrpc_ptr_t> (fdata); // adj matrix pointer
   exrpc_ptr_t gptr = -1;
   try {
-    gptr = exrpc_async(fm_node, set_graph_data<S_MAT1>, f_dptr).get();
+    gptr = exrpc_async(fm_node, (set_graph_data<S_MAT1,DT1>), f_dptr).get();
   }
   catch(std::exception& e) { set_status(true,e.what()); }
   return (long) gptr;
 }
 
-JNIEXPORT jobjectArray JNICALL Java_com_nec_frovedis_Jexrpc_JNISupport_getGraphData
+JNIEXPORT jobjectArray JNICALL Java_com_nec_frovedis_Jexrpc_JNISupport_getGraphEdgeData
   (JNIEnv *env, jclass thisCls, jobject master_node, jlong fdata) {
   auto fm_node = java_node_to_frovedis_node(env, master_node);
   auto f_dptr = static_cast<exrpc_ptr_t> (fdata);
   crs_matrix_local<double> adj_mat;
   try {
-    adj_mat = exrpc_async(fm_node, get_graph_data<graph>, f_dptr).get();
+    adj_mat = exrpc_async(fm_node, (get_graph_edge_data<graph<DT1>,DT1>), f_dptr).get();
   }
   catch(std::exception& e) { set_status(true,e.what()); }
   return to_jDummyEdgeArray(env, adj_mat);
 }
 
-JNIEXPORT jdoubleArray JNICALL 
+JNIEXPORT jdoubleArray JNICALL Java_com_nec_frovedis_Jexrpc_JNISupport_getGraphVertexData
+  (JNIEnv *env, jclass thisCls, jobject master_node, jlong fdata) {
+  auto fm_node = java_node_to_frovedis_node(env, master_node);
+  auto f_dptr = static_cast<exrpc_ptr_t> (fdata);
+  std::vector<double> vertices;
+  try {
+    vertices = exrpc_async(fm_node, get_graph_vertex_data<graph<DT1>>, f_dptr).get();
+  }
+  catch(std::exception& e) { set_status(true,e.what()); }
+  return to_jdoubleArray(env, vertices);
+}
+
+JNIEXPORT jobject JNICALL 
 Java_com_nec_frovedis_Jexrpc_JNISupport_callFrovedisPageRank(JNIEnv *env, jclass thisCls, 
   jobject master_node, jlong fdata, jdouble epsilon, jdouble dfactor, 
   jint max_iter) {
   auto fm_node = java_node_to_frovedis_node(env, master_node);
   auto f_dptr = static_cast<exrpc_ptr_t> (fdata);
   int vb = 0; // no log (default)
-  std::vector<double> result;
+  dummy_graph ret; 
   try {
-    result = exrpc_async(fm_node, frovedis_pagerank<graph>, f_dptr, 
-                         epsilon, dfactor, max_iter, vb).get();
+    ret = exrpc_async(fm_node, frovedis_normalized_pagerank<graph<DT1>>, f_dptr, 
+                      epsilon, dfactor, max_iter, vb).get();
   }
   catch(std::exception& e) { set_status(true,e.what()); }
-  return to_jdoubleArray(env, result);
+  return to_jDummyGraph(env, ret);
 }
 
 JNIEXPORT void JNICALL
 Java_com_nec_frovedis_Jexrpc_JNISupport_callFrovedisSSSP(JNIEnv *env, jclass thisCls,
   jobject master_node, jlong fdata,
-  jintArray dist_arr, jlongArray pred_arr, jlong sz,
+  jdoubleArray dist_arr, jlongArray pred_arr, jlong sz,
   jlong source_vertex) {
   auto fm_node = java_node_to_frovedis_node(env, master_node);
   auto f_dptr = static_cast<exrpc_ptr_t> (fdata);
   int vb = 0; // no log (default)
-  sssp_result res;
+  sssp_result<DT1, DT5> res;
   try {
-    res = exrpc_async(fm_node, frovedis_sssp<graph>, f_dptr,
+    res = exrpc_async(fm_node, (frovedis_sssp<graph<DT1>,DT1,DT5>), f_dptr,
                       source_vertex, vb).get();
   }
   catch(std::exception& e) { set_status(true,e.what()); }
-  checkAssumption(sz == res.nodes_dist.size()); // check in case any size issue
-  env->SetIntArrayRegion(dist_arr, 0, sz, res.nodes_dist.data());
-  env->SetLongArrayRegion(pred_arr, 0, sz, res.nodes_pred.data());
+  checkAssumption(sz == (long)res.distance.size()); // check in case any size issue
+  env->SetDoubleArrayRegion(dist_arr, 0, sz, res.distance.data());
+  env->SetLongArrayRegion(pred_arr, 0, sz,(long*) res.predicator.data());
 }
 
 JNIEXPORT jlongArray JNICALL
 Java_com_nec_frovedis_Jexrpc_JNISupport_callFrovedisBFS(JNIEnv *env, jclass thisCls,
   jobject master_node, jlong fdata,
   jlongArray nodes_in_which_cc,
-  jintArray nodes_dist, jlong sz) {
+  jlongArray nodes_dist, jlong sz) {
   auto fm_node = java_node_to_frovedis_node(env, master_node);
   auto f_dptr = static_cast<exrpc_ptr_t> (fdata);
   int vb = 0; // no log (default)
-  bfs_result res;
+  cc_result<DT5> res;
   try {
-    res = exrpc_async(fm_node, frovedis_bfs<graph>, f_dptr, vb).get();
+    res = exrpc_async(fm_node, (frovedis_bfs<graph<DT1>,DT5>), f_dptr, vb).get();
   }
   catch(std::exception& e) { set_status(true,e.what()); }
-  checkAssumption(sz == res.nodes_dist.size()); // check in case any size issue
-  env->SetIntArrayRegion(nodes_dist, 0, sz, res.nodes_dist.data());
-  env->SetLongArrayRegion(nodes_in_which_cc, 0, sz, res.nodes_in_which_cc.data());
-  return to_jlongArray(env, res.num_nodes_in_each_cc);
+  checkAssumption(sz == (long)res.dist_nodes.size()); // check in case any size issue
+  env->SetLongArrayRegion(nodes_dist, 0, sz, (long*)res.dist_nodes.data());
+  env->SetLongArrayRegion(nodes_in_which_cc, 0, sz, (long*)res.num_nodes_in_which_cc.data());
+  std::vector<long> long_num_nodes_in_each_cc(res.num_nodes_in_each_cc.begin(), res.num_nodes_in_each_cc.end());
+  //return long_num_nodes_in_each_cc;
+  return to_jlongArray(env, long_num_nodes_in_each_cc);
 }
 
 }
