1adb4d84ecf7a9d8d312a87fd3a86a85c28ba448
Takuya Araki <t-araki@dc.jp.nec.com>
2020-07-04 19:01:04 +0900

    fixed KNR/KNC result issue (NTI)

--- src/foreign_if/spark/examples/scala/KNRDemo.scala
+++ src/foreign_if/spark/examples/scala/KNRDemo.scala
@@ -26,69 +26,56 @@ object KNRDemo {
     // initializing Frovedis server with "personalized command", if provided in command line
     if(args.length != 0) FrovedisServer.initialize(args(0))
 
-    val data = Vector(
-       Vectors.dense(-1,1),
-       Vectors.dense(-2, -1),
-       Vectors.dense(-3, -2),
-       Vectors.dense(1,1),
-       Vectors.dense(2, 1),
-       Vectors.dense(3, 2)
-    )
-    val lbl = Vector(10.0, 10.0, 10.0, 20.0, 10.0, 20.0)
-    val zip1 = lbl zip data
-    var lpv = zip1.map( a => LabeledPoint(a._1, a._2) ) // vector of LabeledPoint
-    var d_lp: RDD[LabeledPoint] = sc.parallelize(lpv)  // distributed LabeledPoint
-    val f_lp = new FrovedisLabeledPoint(d_lp, true) // frovedis LabeledPoint
-
     //  -------- data loading from sample file at Spark side--------
     val s_data = sc.textFile("./input/knn_data.txt")
                    .map(s => Vectors.dense(s.split(' ').map(_.toDouble)))
-
     val fdata = new FrovedisRowmajorMatrix(s_data)
-    
-    val algo = new KNeighborsRegressor().setNNeighbors(3)
-                                        .setAlgorithm("brute")
-                                        .setMetric("euclidean")
+
+    val lbl = sc.parallelize(Array(1.2, 0.3, 1.1, 1.9, 1.7, 0.5))
+    val lbv = (lbl zip s_data).map(a => LabeledPoint(a._1, a._2))
+    val f_lp = new FrovedisLabeledPoint(lbv, true) // frovedis LabeledPoint
+
+    val knr = new KNeighborsRegressor().setNNeighbors(3)
+                                       .setAlgorithm("brute")
+                                       .setMetric("euclidean")
 
     println("Using Frovedis data ")
-    algo.run(f_lp)
+    knr.run(f_lp)
     
-    var (dist, ind) = algo.kneighbors(fdata, 3, true);
+    var (dist, ind) = knr.kneighbors(fdata, 3, true);
     println("distance matrix")
     dist.debug_print()
     println("indices matrix")
     ind.debug_print()
     
-    var graph = algo.kneighbors_graph(fdata, 3, "connectivity")
+    var graph = knr.kneighbors_graph(fdata, 3, "connectivity")
     println("knn graph:")
     graph.debug_print()
 
-    var pred: Array[Double] = algo.predict(fdata)
+    var pred: Array[Double] = knr.predict(fdata)
     println("predicted output: ")
     for(e <- pred) print(e + " ")
     
-    var d_lbl = sc.parallelize(lbl) // distributed labels
-    var score = algo.score(fdata, DoubleDvector.get(d_lbl))
+    var score = knr.score(fdata, DoubleDvector.get(lbl))
     println("score: " + score)
 
-
     println("Using Spark data")
-    algo.run(d_lp)
+    knr.run(lbv)
 
-    var (dist2, ind2) = algo.kneighbors(s_data);
+    var (dist2, ind2) = knr.kneighbors(s_data);
     println("Distance Row matrix: ")
     dist2.rows.collect.foreach(println)
     println("Indices Row matrix:")
     ind2.rows.collect.foreach(println)
 
-    var pred2: Array[Double] = algo.predict(s_data)
+    var pred2: Array[Double] = knr.predict(s_data)
     println("predicted output: ")
     for(e <- pred) print(e + " ")
 
-    var score2 = algo.score(d_lp)
+    var score2 = knr.score(lbv)
     println("score: " + score)
 
-    algo.release()
+    knr.release()
     
     FrovedisServer.shut_down()
     sc.stop()
