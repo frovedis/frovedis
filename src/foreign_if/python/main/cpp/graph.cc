e447a56018c66201339c76acc847d6dd3b1b9be9
Takuya Araki <t-araki@dc.jp.nec.com>
2020-07-04 19:05:29 +0900

    restructured python networkx wrapper (NTI)

--- src/foreign_if/python/main/cpp/graph.cc
+++ src/foreign_if/python/main/cpp/graph.cc
@@ -11,7 +11,7 @@ extern "C" {
     //int vb = 0; //no log (default=0)
     exrpc_ptr_t result = -1;
     try{
-      result = exrpc_async(fm_node,set_graph_data<S_MAT1>,f_dptr).get();
+      result = exrpc_async(fm_node,(set_graph_data<S_MAT1,DT1>),f_dptr).get();
     }
     catch(std::exception& e) { set_status(true,e.what()); }
     return (long)result;
@@ -21,10 +21,9 @@ extern "C" {
     auto f_dptr = static_cast<exrpc_ptr_t> (proxy);
     dummy_matrix adj_mat;
     try {
-      adj_mat = exrpc_async(fm_node, get_graph_data_dummy_matrix<graph>, f_dptr).get();
+      adj_mat = exrpc_async(fm_node, get_graph_data_dummy_matrix<graph<DT1>>, f_dptr).get();
     }
     catch(std::exception& e) { set_status(true,e.what()); }
-    //return to_py_dummy_edge_array(adj_mat);
     return to_py_dummy_matrix(adj_mat);
   }
   PyObject* call_frovedis_pagerank(const char* host, int port, long proxy,
@@ -36,7 +35,7 @@ extern "C" {
     int vb = 0; //no log (default=0)
     std::vector<double> result;
     try{
-      result = exrpc_async(fm_node, frovedis_pagerank<graph>, f_dptr,
+      result = exrpc_async(fm_node, frovedis_pagerank<graph<DT1>>, f_dptr,
                        epsilon, dfactor, max_iter, vb).get();
     }
     catch(std::exception& e) { set_status(true,e.what()); }
@@ -48,16 +47,16 @@ extern "C" {
     exrpc_node fm_node(host, port);
     auto f_dptr = static_cast<exrpc_ptr_t> (proxy);
     int vb = 0; //no log (default=0)
-    sssp_result result;
+    sssp_result<DT1,DT5> result;
     try{
-      result = exrpc_async(fm_node, frovedis_sssp<graph>, f_dptr,
+      result = exrpc_async(fm_node, (frovedis_sssp<graph<DT1>,DT1,DT5>), f_dptr,
                        source, vb).get();
     }
     catch(std::exception& e) { set_status(true,e.what()); }
-    checkAssumption(result.nodes_dist.size() == num_vertices);
-    checkAssumption(result.nodes_pred.size() == num_vertices);
-    auto resdistp = result.nodes_dist.data();
-    auto respredp = result.nodes_pred.data();
+    checkAssumption(result.distance.size() == num_vertices);
+    checkAssumption(result.predicator.size() == num_vertices);
+    auto resdistp = result.distance.data();
+    auto respredp = result.predicator.data();
     for(size_t i = 0; i < num_vertices; ++i){
       dist[i] = resdistp[i];
       pred[i] = respredp[i];
@@ -65,32 +64,45 @@ extern "C" {
   }
   PyObject* call_frovedis_bfs(const char* host, int port, long proxy,
                               long* nodes_in_which_cc, 
-                              int* nodes_dist, long num_vertices) {
+                              long* nodes_dist, long num_vertices) {
     ASSERT_PTR(host);
     exrpc_node fm_node(host, port);
     auto f_dptr = static_cast<exrpc_ptr_t> (proxy);
     int vb = 0; //no log (default=0)
-    bfs_result result;
+    cc_result<DT5> result;
     try{
-      result = exrpc_async(fm_node, frovedis_bfs<graph>, f_dptr, vb).get();
+      result = exrpc_async(fm_node, (frovedis_bfs<graph<DT1>,DT5>), f_dptr, vb).get();
     }
     catch(std::exception& e) { set_status(true,e.what());}
-    checkAssumption(result.nodes_dist.size() == num_vertices);
-    checkAssumption(result.nodes_in_which_cc.size() == num_vertices);
-    auto resdistp = result.nodes_dist.data();
-    auto reswhichp = result.nodes_in_which_cc.data();
+    checkAssumption(result.dist_nodes.size() == num_vertices);
+    checkAssumption(result.num_nodes_in_which_cc.size() == num_vertices);
+    auto resdistp = result.dist_nodes.data();
+    auto reswhichp = result.num_nodes_in_which_cc.data();
     for(size_t i = 0; i < num_vertices; ++i){
       nodes_dist[i] = resdistp[i];
       nodes_in_which_cc[i] = reswhichp[i];
     }
-    return to_python_long_list(result.num_nodes_in_each_cc);
+    return to_python_llong_list(result.num_nodes_in_each_cc); 
   }
+
+  long copy_graph_py(const char* host, int port, long proxy) {
+    ASSERT_PTR(host);
+    exrpc_node fm_node(host, port);
+    auto f_dptr = static_cast<exrpc_ptr_t> (proxy);
+    exrpc_ptr_t ret = -1;
+    try {
+      ret = exrpc_async(fm_node, copy_graph<graph<DT1>>, f_dptr).get();
+    }
+    catch(std::exception& e) { set_status(true,e.what()); }
+    return (long)ret;
+  }
+
   void show_graph_py(const char* host, int port, long proxy){
     ASSERT_PTR(host);
     exrpc_node fm_node(host, port);
     auto f_dptr = static_cast<exrpc_ptr_t> (proxy);
     try{
-      exrpc_oneway(fm_node, show_graph<graph>, f_dptr);
+      exrpc_oneway(fm_node, show_graph<graph<DT1>>, f_dptr);
     }
     catch(std::exception& e) {set_status(true, e.what());}
   }
@@ -99,7 +111,7 @@ extern "C" {
     exrpc_node fm_node(host, port);
     auto f_dptr = static_cast<exrpc_ptr_t> (proxy);
     try{
-      exrpc_oneway(fm_node, release_graph<graph>, f_dptr);
+      exrpc_oneway(fm_node, release_graph<graph<DT1>>, f_dptr);
     }
     catch(std::exception& e) {set_status(true, e.what());}
   }
@@ -110,7 +122,7 @@ extern "C" {
     std::string f_name(fname);
     auto f_dptr = static_cast<exrpc_ptr_t> (proxy);
     try{
-      exrpc_oneway(fm_node, save_graph<graph>, f_dptr, f_name);
+      exrpc_oneway(fm_node, save_graph<graph<DT1>>, f_dptr, f_name);
     }
     catch(std::exception& e) {set_status(true, e.what());}
   }
@@ -121,20 +133,9 @@ extern "C" {
     std::string f_name(fname);
     dummy_graph ret;
     try{
-      ret = exrpc_async(fm_node, load_graph<graph>, f_name).get();
+      ret = exrpc_async(fm_node, (load_graph<graph<DT1>,DT1>), f_name).get();
     }
     catch(std::exception& e) {set_status(true, e.what());}
     return to_py_dummy_graph(ret);
-  }
-  long copy_graph_py(const char* host, int port, long proxy) {
-    ASSERT_PTR(host);
-    exrpc_node fm_node(host, port);
-    auto f_dptr = static_cast<exrpc_ptr_t> (proxy);
-    exrpc_ptr_t ret = -1;
-    try {
-      ret = exrpc_async(fm_node, copy_graph<graph>, f_dptr).get();
-      }
-    catch(std::exception& e) { set_status(true,e.what()); }
-    return (long)ret;
   }
 }
