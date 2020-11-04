39460260506557adfdcf4e61ca7f675e3a86aae6
Takuya Araki <t-araki@dc.jp.nec.com>
2020-11-04 17:01:10 +0900

    fixed edgetype handling issue in GraphLoader, double type weight handling issue, etc. (NTI)

--- src/foreign_if/python/main/cpp/graph.cc
+++ src/foreign_if/python/main/cpp/graph.cc
@@ -3,7 +3,6 @@
 #include "short_hand_sparse_type.hpp"
 
 extern "C" {
-  //pagerank
   long set_graph_data(const char* host, int port, long proxy){
     ASSERT_PTR(host);
     exrpc_node fm_node(host, port);
@@ -16,6 +15,7 @@ extern "C" {
     catch(std::exception& e) { set_status(true,e.what()); }
     return (long)result;
   }
+
   PyObject* get_graph_data(const char* host, int port, long proxy) {
     exrpc_node fm_node(host, port);
     auto f_dptr = static_cast<exrpc_ptr_t> (proxy);
@@ -26,6 +26,7 @@ extern "C" {
     catch(std::exception& e) { set_status(true,e.what()); }
     return to_py_dummy_matrix(adj_mat);
   }
+
   PyObject* call_frovedis_pagerank(const char* host, int port, long proxy,
                                   double epsilon, double dfactor,
                                   int max_iter){
@@ -41,8 +42,10 @@ extern "C" {
     catch(std::exception& e) { set_status(true,e.what()); }
     return to_python_double_list(result);
   }
+
   void call_frovedis_sssp(const char* host, int port, long proxy,
-                        int* dist, long* pred, long num_vertices, long source){
+                          double* dist, long* pred, 
+                          long num_vertices, long source){
     ASSERT_PTR(host);
     exrpc_node fm_node(host, port);
     auto f_dptr = static_cast<exrpc_ptr_t> (proxy);
@@ -53,36 +56,72 @@ extern "C" {
                        source, vb).get();
     }
     catch(std::exception& e) { set_status(true,e.what()); }
-    checkAssumption(result.distance.size() == num_vertices);
-    checkAssumption(result.predicator.size() == num_vertices);
-    auto resdistp = result.distance.data();
-    auto respredp = result.predicator.data();
-    for(size_t i = 0; i < num_vertices; ++i){
+    checkAssumption(result.distances.size() == num_vertices);
+    checkAssumption(result.predecessors.size() == num_vertices);
+    auto resdistp = result.distances.data();
+    auto respredp = result.predecessors.data();
+    for(size_t i = 0; i < num_vertices; ++i) {
       dist[i] = resdistp[i];
-      pred[i] = respredp[i];
+      pred[i] = (long) respredp[i];
     }
   }
-  PyObject* call_frovedis_bfs(const char* host, int port, long proxy,
-                              long* nodes_in_which_cc, 
-                              long* nodes_dist, long num_vertices) {
+
+  void call_frovedis_bfs(const char* host, int port, long proxy,
+                         long* dist, long* pred,
+                         long num_vertices, long source,
+                         int opt_level, double hyb_threshold) {
+    ASSERT_PTR(host);
+    exrpc_node fm_node(host, port);
+    auto f_dptr = static_cast<exrpc_ptr_t> (proxy);
+    int vb = 0; //no log (default=0)
+    bfs_result<DT5> result;
+    try{
+      result = exrpc_async(fm_node, (frovedis_bfs<graph<DT1>,DT5>), f_dptr,
+                       source, opt_level, hyb_threshold, vb).get();
+    }
+    catch(std::exception& e) { set_status(true,e.what()); }
+    checkAssumption(result.distances.size() == num_vertices);
+    checkAssumption(result.predecessors.size() == num_vertices);
+    auto resdistp = result.distances.data();
+    auto respredp = result.predecessors.data();
+    auto lmax = std::numeric_limits<long>::max();
+    auto uimax = std::numeric_limits<size_t>::max();
+    for(size_t i = 0; i < num_vertices; ++i) {
+      dist[i] = (resdistp[i] == uimax) ? lmax : (long) resdistp[i];
+      pred[i] = (long) respredp[i];
+    }
+  }
+
+  PyObject* call_frovedis_cc(const char* host, int port, long proxy,
+                             long* nodes_in_which_cc, 
+                             long* nodes_dist, long num_vertices,
+                             int opt_level, double hyb_threshold) {
     ASSERT_PTR(host);
     exrpc_node fm_node(host, port);
     auto f_dptr = static_cast<exrpc_ptr_t> (proxy);
     int vb = 0; //no log (default=0)
     cc_result<DT5> result;
     try{
-      result = exrpc_async(fm_node, (frovedis_bfs<graph<DT1>,DT5>), f_dptr, vb).get();
+      result = exrpc_async(fm_node, (frovedis_cc<graph<DT1>,DT5>), 
+                           f_dptr, opt_level, hyb_threshold, vb).get();
     }
     catch(std::exception& e) { set_status(true,e.what());}
     checkAssumption(result.distances.size() == num_vertices);
     checkAssumption(result.num_nodes_in_which_cc.size() == num_vertices);
     auto resdistp = result.distances.data();
     auto reswhichp = result.num_nodes_in_which_cc.data();
+    auto lmax = std::numeric_limits<long>::max();
+    auto uimax = std::numeric_limits<size_t>::max();
     for(size_t i = 0; i < num_vertices; ++i){
-      nodes_dist[i] = resdistp[i];
-      nodes_in_which_cc[i] = reswhichp[i];
+      nodes_dist[i] = (resdistp[i] == uimax) ? lmax : (long) resdistp[i];
+      nodes_in_which_cc[i] = (reswhichp[i] == uimax) ? lmax : (long) reswhichp[i];
     }
-    return to_python_llong_list(result.num_nodes_in_each_cc); 
+    std::vector<long> ret(2 * result.num_cc);
+    for(size_t i = 0; i < result.num_cc; ++i) {
+      ret[2 * i] = result.root_in_each_cc[i];
+      ret[2 * i + 1] = result.num_nodes_in_each_cc[i];
+    }
+    return to_python_long_list(ret);
   }
 
   long copy_graph_py(const char* host, int port, long proxy) {
@@ -106,6 +145,7 @@ extern "C" {
     }
     catch(std::exception& e) {set_status(true, e.what());}
   }
+
   void release_graph_py(const char* host, int port, long proxy){
     ASSERT_PTR(host);
     exrpc_node fm_node(host, port);
@@ -115,6 +155,7 @@ extern "C" {
     }
     catch(std::exception& e) {set_status(true, e.what());}
   }
+
   void save_graph_py(const char* host, int port, long proxy,
                      const char* fname){
     ASSERT_PTR(host);
@@ -126,6 +167,7 @@ extern "C" {
     }
     catch(std::exception& e) {set_status(true, e.what());}
   }
+
   PyObject* load_graph_from_text_file(const char* host, int port,
             const char* fname){
     ASSERT_PTR(host);
