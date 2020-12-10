b90f855c8a7ba28f62cbafa55bea29481b3502f1
Takuya Araki <t-araki@dc.jp.nec.com>
2020-12-10 18:50:34 +0900

    improved linear svm python demo (NTI)

--- src/foreign_if/python/examples/svr_demo.py
+++ src/foreign_if/python/examples/svr_demo.py
@@ -2,9 +2,10 @@
 
 import sys
 import numpy as np
-from sklearn.datasets import load_boston
-from frovedis.exrpc.server import FrovedisServer
-from frovedis.mllib.svm import LinearSVR as SVR
+np.set_printoptions(threshold=5)
+
+#from sklearn.svm import LinearSVR
+from frovedis.mllib.svm import LinearSVR
 
 # initializing the Frovedis server
 argvs = sys.argv
@@ -12,18 +13,22 @@ argc = len(argvs)
 if (argc < 2):
     print ('Please give frovedis_server calling command as the first argument \n(e.g. "mpirun -np 2 -x /opt/nec/nosupport/frovedis/ve/bin/frovedis_server")')
     quit()
+
+from frovedis.exrpc.server import FrovedisServer
 FrovedisServer.initialize(argvs[1])
 
+# regression data
+from sklearn.datasets import load_boston
 mat, lbl = load_boston(return_X_y=True)
 
 # fitting input matrix and label on svm regression object
-svr = SVR(loss='epsilon_insensitive', epsilon = 0.1,
-          max_iter=1000, penalty='l2').fit(mat,lbl)
+svr = LinearSVR(loss='epsilon_insensitive', epsilon = 0.1,
+                max_iter=1000, penalty='l2').fit(mat, lbl)
 
 # predicting on loaded model
 print("predicting on trained model: ")
-print(svr.predict(mat[:10]))
-print("score: %.2f" % svr.score(mat[:10], lbl[:10]))
+print(svr.predict(mat))
+print("preduction score: %.2f" % svr.score(mat, lbl))
 
 # saving the model
 svr.save("./out/SVRModel")
