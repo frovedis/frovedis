6c6b76c668da62b90da8e65daaa2153937e1835b
Takuya Araki <t-araki@dc.jp.nec.com>
2020-12-10 18:54:48 +0900

    improved dtree python demo; corrected result of dtree predict_proba() same as in sklearn (NTI)

--- src/foreign_if/python/examples/dt_demo.py
+++ src/foreign_if/python/examples/dt_demo.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 
 import sys
-import numpy as np
-import pandas as pd
-from frovedis.exrpc.server import FrovedisServer
-from frovedis.mllib.tree import DecisionTreeClassifier
-from frovedis.mllib.tree import DecisionTreeRegressor
 
-#Objective: Run without error
+import numpy as np
+np.set_printoptions(threshold=5)
+
+#from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+from frovedis.mllib.tree import DecisionTreeClassifier, DecisionTreeRegressor
 
 # initializing the Frovedis server
 argvs = sys.argv
@@ -15,51 +14,41 @@ argc = len(argvs)
 if (argc < 2):
     print ('Please give frovedis_server calling command as the first argument \n(e.g. "mpirun -np 2 -x /opt/nec/nosupport/frovedis/ve/bin/frovedis_server")')
     quit()
+
+from frovedis.exrpc.server import FrovedisServer
 FrovedisServer.initialize(argvs[1])
 
-mat = pd.DataFrame([[10, 0, 1, 0, 0, 1, 0],
-                    [0, 1, 0, 1, 0, 1, 0],
-                    [0, 1, 0, 0, 1, 0, 1],
-                    [1, 0, 0, 1, 0, 1, 0]],dtype=np.float64)
-lbl = np.array([0, 1, 1, 0]) 
+# classification data
+from sklearn.datasets import load_breast_cancer
+mat, lbl = load_breast_cancer(return_X_y=True)
 
 # fitting input matrix and label on DecisionTree Classifier object
-dtc1 = DecisionTreeClassifier(criterion='gini', splitter='best', max_depth=5, 
-                              min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, 
-                              max_features=None, random_state=None, max_leaf_nodes=1, 
-                              min_impurity_decrease=0.0,
-                              class_weight=None, presort=False, verbose = 0)
-dtc = dtc1.fit(mat,lbl)
-dtc.debug_print()
+dtc = DecisionTreeClassifier(criterion='gini', max_depth=5)
+dtc.fit(mat,lbl)
+#dtc.debug_print()
 
 # predicting on train model
 print("predicting on DecisionTree classifier model: ")
-dtcm = dtc.predict(mat)
-print (dtcm)
+print(dtc.predict(mat))
+print("predicting probability on DecisionTree classifier model: ")
 print (dtc.predict_proba(mat))
-print("Accuracy score for predicted DecisionTree Classifier model")
-print (dtc.score(mat,lbl))
+print("prediction accuracy: %.4f" % (dtc.score(mat, lbl)))
+
+# regression data
+from sklearn.datasets import load_boston
+mat, lbl = load_boston(return_X_y=True)
 
 # fitting input matrix and label on DecisionTree Regressor object
-dtr1 = DecisionTreeRegressor(criterion='mse', splitter='best',
-                             max_depth=5, min_samples_split=2, min_samples_leaf=1,
-                             min_weight_fraction_leaf=0.0, max_features=None, random_state=None,
-                             max_leaf_nodes=1, min_impurity_decrease=0.0, min_impurity_split=None,
-                             class_weight=None, presort=False, verbose = 0)
-lbl1 = np.array([1.2,0.3,1.1,1.9])
-dtr = dtr1.fit(mat,lbl1)
-dtr.debug_print()
+dtr = DecisionTreeRegressor(criterion='mse', max_depth=5)
+dtr.fit(mat, lbl)
+#dtr.debug_print()
 
 # predicting on train model
 print("predicting on  DecisionTree Regressor model: ")
-dtrm =  dtr.predict(mat)
-print (dtrm)
-print("Root mean square for predicted DecisionTree Regressor model")
-print (dtr.score(mat,lbl1))
-
+print(dtr.predict(mat))
+print("prediction score: %.4f" % (dtr.score(mat, lbl)))
 
 #clean-up
-dtc.release()
-dtr.release()
+#dtc.release()
+#dtr.release()
 FrovedisServer.shut_down()
-
