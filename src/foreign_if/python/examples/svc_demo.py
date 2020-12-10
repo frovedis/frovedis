285c6c97f0f2ef8bfd64672ac24efe374250eb0e
Takuya Araki <t-araki@dc.jp.nec.com>
2020-12-10 18:54:19 +0900

    imporved SVM kernel python demo (NTI)

--- src/foreign_if/python/examples/svc_demo.py
+++ src/foreign_if/python/examples/svc_demo.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
 import sys
+
 import numpy as np
-from frovedis.exrpc.server import FrovedisServer
+np.set_printoptions(threshold=5)
+
+#from sklearn.svm import SVC
 from frovedis.mllib.svm import SVC
 
 # initializing the Frovedis server
@@ -11,42 +14,39 @@ argc = len(argvs)
 if (argc < 2):
     print ('Please give frovedis_server calling command as the first argument \n(e.g. "mpirun -np 2 -x /opt/nec/nosupport/frovedis/ve/bin/frovedis_server")')
     quit()
+
+from frovedis.exrpc.server import FrovedisServer
 FrovedisServer.initialize(argvs[1])
 
-#from sklearn.datasets import load_boston
-#mat, lbl = load_boston(return_X_y=True)
+# classification data
+from sklearn.datasets import load_breast_cancer
+mat, lbl = load_breast_cancer(return_X_y=True)
 
-mat = np.array([[10, 0, 1, 0, 0, 1, 0],
-                    [0, 1, 0, 1, 0, 1, 0],
-                    [0, 1, 0, 0, 1, 0, 1],
-                    [1, 0, 0, 1, 0, 1, 0]],dtype=np.float64)
-lbl = np.array([1, -1, 1, -1],dtype=np.float64)
-
-
-# fitting input matrix and label on svm regression object
-svc = SVC(C=1.0, kernel='rbf', degree=3, gamma='scale', coef0=0.0,
-          shrinking=True, probability=True, tol=0.001, cache_size=128,
-          class_weight=None, verbose=False, max_iter=-1).fit(mat,lbl)
+# fitting input matrix and label on svm kernel object
+#svc = SVC(C=1.0, kernel='linear', degree=3, probability=True)
+svc = SVC(C=1.0, kernel='rbf', degree=3, probability=True)
+svc.fit(mat, lbl)
 
 # predicting on loaded model
 print("predicting on trained model: ")
-print(svc.predict(mat[:10]))
+print(svc.predict(mat))
+
+print("predict_proba on trained model: ")
+print(svc.predict_proba(mat))
 
 print("support_vectors_ on trained model: ")
 print(svc.support_vectors_)
+
 print("support_ on trained model: ")
 print(svc.support_)
-print("predict_proba on trained model: ")
-print(svc.predict_proba(mat[:10]))
 
 # saving the model
 svc.save("./out/SVCModel")
 
 # loading the same model
-svc2 = svc.load("./out/SVCModel")
-print(svc2.predict(mat))
+svc.load("./out/SVCModel")
+print("predicting on loaded model: ")
+print(svc.predict(mat))
 
 svc.release()
-
 FrovedisServer.shut_down()
-
