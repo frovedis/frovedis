4c0fd47590dbd1d64d8ffa734da4da3de4e84e61
Takuya Araki <takuy_araki@nec.com>
2020-12-28 15:58:10 +0900

    added shrink in svd, kmeans and logistic regression (NTI)

--- src/foreign_if/server/expose_ml.cc
+++ src/foreign_if/server/expose_ml.cc
@@ -5,6 +5,16 @@
 
 using namespace frovedis;
 
+RegType get_regularizer(int regType) {
+  RegType rtype = ZERO;
+  if (regType == 0) rtype = ZERO;
+  else if (regType == 1) rtype = L1;
+  else if (regType == 2) rtype = L2;
+  else REPORT_ERROR(USER_ERROR, 
+       "Currently frovedis supports ZERO/L1/L2 regularizer!");
+  return rtype;
+}
+
 void frovedis_w2v_train(std::string& encode,
                         std::string& weight,
                         std::string& count,
@@ -29,6 +39,11 @@ void expose_frovedis_ml_functions() {
   expose((frovedis_lr_lbfgs<DT1,S_MAT15>)); // python case
   expose((frovedis_lr_lbfgs<DT2,S_MAT24>)); // python case
   expose((frovedis_lr_lbfgs<DT2,S_MAT25>)); // python case
+  expose((frovedis_lr_shrink_sgd<DT1,S_MAT1>));    // spark case
+  expose((frovedis_lr_shrink_sgd<DT1,S_MAT14>));   // python case
+  expose((frovedis_lr_shrink_sgd<DT1,S_MAT15>));   // python case
+  expose((frovedis_lr_shrink_sgd<DT2,S_MAT24>));   // python case
+  expose((frovedis_lr_shrink_sgd<DT2,S_MAT25>));   // python case
   // (2) linear svm
   expose((frovedis_svm_sgd<DT1,D_MAT1>));    // both spark and python case
   expose((frovedis_svm_sgd<DT2,D_MAT2>));    // python case
