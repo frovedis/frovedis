6baade4716ef3b510339ed7d16d05449d41b6b63
Takuya Araki <takuy_araki@nec.com>
2020-12-28 15:15:24 +0900

    BFS depth limit support changes for python (NTI)

--- src/foreign_if/python/examples/graph_bfs.py
+++ src/foreign_if/python/examples/graph_bfs.py
@@ -1,8 +1,8 @@
 import sys
 import numpy as np
 from frovedis.exrpc.server import *
-import frovedis.graph as fnx
 import networkx as nx
+import frovedis.graph as fnx
 
 # initializing the Frovedis server
 argvs = sys.argv
@@ -11,20 +11,30 @@ if (argc < 2):
     print ('Please give frovedis_server calling command as the first argument \n(e.g. "mpirun -np 2 -x /opt/nec/nosupport/frovedis/ve/bin/frovedis_server")')
     quit()
 
-source = 1
-
+source = 1 
+depth = 1
 fname = 'input/cit-Patents_10.txt' 
 
 FrovedisServer.initialize(argvs[1])
-#frov_graph = fnx.read_edgelist(fname, nodetype=np.int32)
-frov_graph = fnx.read_edgelist(fname, nodetype=np.int32, create_using=nx.DiGraph())
-frov_graph.debug_print()
+G_nx = nx.read_edgelist(fname, nodetype=np.int32, create_using=nx.DiGraph())
+G = fnx.read_edgelist(fname, nodetype=np.int32, create_using=nx.DiGraph())
+#G.debug_print()
 
-#returns dict of lists containing shortest path from source to all other nodes
-ret = fnx.bfs(frov_graph, source) 
-print("Frovedis bfs traversal path: ")
-print(ret)
+print ("Frovedis BFS edges: ", list(fnx.bfs_edges(G, source, depth_limit=depth)))
+print ("NetworkX BFS edges: ", list(nx.bfs_edges(G_nx, source, depth_limit=depth)))
 
-frov_graph.release()
-# Shutting down the Frovedis server
+print ("Edges in Frovedis bfs_tree: ", fnx.bfs_tree(G, source, depth_limit=depth).number_of_edges())
+print ("Edges in NetworkX bfs_tree: ", nx.bfs_tree(G_nx, source, depth_limit=depth).number_of_edges())
+
+print ("Frovedis bfs_predecessors: ", list(fnx.bfs_predecessors(G, source, depth_limit=depth)))
+print ("NetworkX bfs_predecessors: ", list(nx.bfs_predecessors(G_nx, source, depth_limit=depth)))
+
+print ("Frovedis bfs_successors: ", list(fnx.bfs_successors(G, source, depth_limit=depth)))
+print ("NetworkX bfs_successors: ", list(nx.bfs_successors(G_nx, source, depth_limit=depth)))
+
+print ("Frovedis descendants at distance:", fnx.descendants_at_distance(G, source, distance=depth))
+print ("NetworkX descendants at distance:", nx.descendants_at_distance(G_nx, source, distance=depth))
+
+# clean-up at the server side
+G.release()
 FrovedisServer.shut_down()
