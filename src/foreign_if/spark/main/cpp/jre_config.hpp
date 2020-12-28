f3ef2a17119ee5aeb8ba4d239e83c2a393fdd943
Takuya Araki <takuy_araki@nec.com>
2020-12-28 15:48:29 +0900

    Added TSNE Scala wrapper (NTI)

--- src/foreign_if/spark/main/cpp/jre_config.hpp
+++ src/foreign_if/spark/main/cpp/jre_config.hpp
@@ -7,6 +7,7 @@
 #define JRE_PATH_Node "com/nec/frovedis/Jexrpc/Node"
 #define JRE_PATH_DummyGesvdResult "com/nec/frovedis/Jmatrix/DummyGesvdResult"
 #define JRE_PATH_DummyPCAResult "com/nec/frovedis/Jmatrix/DummyPCAResult"
+#define JRE_PATH_DummyTSNEResult "com/nec/frovedis/Jmllib/DummyTSNEResult"
 #define JRE_PATH_DummyKNNResult "com/nec/frovedis/Jmllib/DummyKNNResult"
 #define JRE_PATH_DummyGetrfResult "com/nec/frovedis/Jmatrix/DummyGetrfResult"
 #define JRE_PATH_DummyMatrix "com/nec/frovedis/Jmatrix/DummyMatrix"
@@ -14,6 +15,8 @@
 #define JRE_PATH_IntDoublePair "com/nec/frovedis/Jmllib/IntDoublePair"
 #define JRE_PATH_DummyEdge "com/nec/frovedis/Jgraph/DummyEdge"
 #define JRE_PATH_DummyGraph "com/nec/frovedis/Jgraph/DummyGraph"
+#define JRE_PATH_bfs_result "com/nec/frovedis/graphx/bfs_result"
+#define JRE_PATH_sssp_result "com/nec/frovedis/graphx/sssp_result"
 #define JRE_PATH_DummyLDAResult "com/nec/frovedis/Jmllib/DummyLDAResult"
 #define JRE_PATH_DummyLDAModel "com/nec/frovedis/Jmllib/DummyLDAModel"
 
