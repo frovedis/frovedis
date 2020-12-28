e899daa517910dd1d6b7dc5ea7e843f70240a13a
Takuya Araki <takuy_araki@nec.com>
2020-12-28 16:04:46 +0900

    renamed result structures (NTI)

--- src/foreign_if/server/exrpc_util_headers.hpp
+++ src/foreign_if/server/exrpc_util_headers.hpp
@@ -2,15 +2,12 @@
 #define _EXRPC_UTIL_HPP_
 
 #include "../exrpc/exrpc_expose.hpp"
+#include "model_tracker.hpp"
 #include "frovedis_mem_pair.hpp"
 #include "dummy_matrix.hpp"
 #include "dummy_graph.hpp"
 #include "dummy_model.hpp"
-#include "pca_result.hpp"
-#include "tsne_result.hpp"
-#include "knn_result.hpp"
+#include "ml_result.hpp"
 #include "lda_result.hpp"
-#include "scalapack_result.hpp"
-#include "model_tracker.hpp"
 
 #endif
