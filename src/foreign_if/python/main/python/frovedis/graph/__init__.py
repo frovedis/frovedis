3660a5d5797365883c271118744bea0457697da1
Takuya Araki <takuy_araki@nec.com>
2020-12-28 15:07:19 +0900

    added bfs_edges and descendants_at_distance wrapper in python graph (NTI)

--- src/foreign_if/python/main/python/frovedis/graph/__init__.py
+++ src/foreign_if/python/main/python/frovedis/graph/__init__.py
@@ -4,8 +4,13 @@ from .graph import Graph, to_scipy_sparse_matrix
 from .GraphLoader import read_edgelist
 from .Pagerank import pagerank
 from .ConnectedComponents import connected_components
-from .traversal import single_source_shortest_path, bfs
+from .traversal import single_source_shortest_path
+from .traversal import bfs, bfs_edges, descendants_at_distance
+from .traversal import bfs_tree, bfs_predecessors, bfs_successors
 
-__all__ = [Graph, to_scipy_sparse_matrix, read_edgelist,
-           pagerank, connected_components,
-           single_source_shortest_path, bfs]
+__all__ = ["Graph", "to_scipy_sparse_matrix", "read_edgelist",
+           "pagerank", 
+           "connected_components",
+           "single_source_shortest_path", 
+           "bfs", "bfs_edges", "descendants_at_distance",
+           "bfs_tree", "bfs_predecessors", "bfs_successors"]
