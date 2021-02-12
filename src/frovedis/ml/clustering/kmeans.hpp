47165ce9e7b6c5bb2a6420843ab30d22a92479e7
Takuya Araki <takuy_araki@nec.com>
2021-02-12 18:51:11 +0900

    supported n_init in kmeans; added transform, fit_transform; restructured kmeans_assign_cluster code, fixed n_iter_ computation bug, etc. (NTI)

--- src/frovedis/ml/clustering/kmeans.hpp
+++ src/frovedis/ml/clustering/kmeans.hpp
@@ -9,170 +9,164 @@
 
 namespace frovedis {
 
-  template <class MATRIX, class T>
-  std::vector<int>
-  invoke_kmeans_assign_cluster(
-    MATRIX& mat, 
-    rowmajor_matrix_local<T>& centroid,
-    T& inertia,
-    bool need_inertia) {
-    auto lbl = kmeans_assign_cluster(mat, centroid, inertia, need_inertia); 
-    return lbl;
+template <class T>
+struct KMeans { 
+  KMeans(int k,
+         int n_init = 1,
+         int max_iter = 300,
+         double eps = 0.0001,
+         long seed = 0,
+         bool shrink = false){
+    this->k = k;
+    this->n_init = n_init;
+    this->max_iter = max_iter;
+    this->eps = eps;
+    this->seed = seed;
+    this->use_shrink = shrink;
+    this->is_fitted = false;
   }
 
-  template <class T>
-  struct KMeans { 
-    KMeans(int k,
-	   int max_iter = 100,
-	   double eps = 0.01, 
-           long seed = 0,
-           bool shrink = false){
-      this->k = k;
-      this->max_iter = max_iter;
-      this->eps = eps;
-      this->seed = seed;
-      this->use_shrink = shrink;
-      this->is_fitted = false;
+  KMeans<T>& 
+  set_k(int k) {
+    std::string msg = "expected a positive k; received: " + STR(k) + "\n";
+    require(k > 0, msg);
+    this->k = k;
+    return *this;
+  }
+    
+  KMeans<T>&
+  set_max_iter(int max_iter) {
+    std::string msg = "expected a positive max_iter; received: " + STR(max_iter) + "\n";
+    require(max_iter > 0, msg);
+    this->max_iter = max_iter;
+    return *this;
+  }
+
+  KMeans<T>&
+  set_n_init(int n_init) {
+    std::string msg = "expected a positive n_init; received: " + STR(n_init) + "\n";
+    require(n_init > 0, msg);
+    this->n_init = n_init;
+    return *this;
+  }
+
+  KMeans<T>& 
+  set_eps(double eps) { // eps >= 0.0 && eps <= 1.0
+    std::string msg = "expected eps within the range of 0.0 to 1.0; received: " + STR(eps) + "\n";
+    require(eps >= 0.0 && eps <= 1.0, msg);
+    this->eps = eps;
+    return *this;
+  }
+
+  KMeans<T>& 
+  set_seed(long seed) { 
+    std::string msg = "expected zero or a positive seed; received: " + STR(seed) + "\n";
+    require(seed >= 0, msg);
+    this->seed = seed;
+    return *this;
+  }
+
+  KMeans<T>&
+  set_use_shrink(bool shrink) {
+    this->use_shrink = shrink;
+    return *this;
+  }
+
+  KMeans<T>& 
+  set_params(std::vector<std::pair<std::string, param_t>>& data) {
+    std::string msg = "";
+    for (auto& e: data) {
+      auto param = e.first;
+      auto val = e.second;
+      if(param == "k") {
+        set_k(val.get<int>());
+        msg += "k: " + val.tt + "; ";
+      }
+      else if(param == "n_init") {
+        set_n_init(val.get<int>());
+        msg += "n_init: " + val.tt + "; ";
+      }
+      else if(param == "max_iter") {
+        set_max_iter(val.get<int>());
+        msg += "max_iter: " + val.tt + "; ";
+      }
+      else if(param == "eps") {
+        set_eps(val.get<double>());
+        msg += "eps: " + val.tt + "; ";
+      }
+      else if(param == "seed") {
+        set_seed(val.get<long>());
+        msg += "seed: " + val.tt + "; ";
+      }
+      else if(param == "use_shrink") {
+        set_use_shrink(val.get<bool>());
+        msg += "use_shrink: " + val.tt + "; ";
+      }
+      else REPORT_ERROR(USER_ERROR, "[KMeans] Unknown parameter: '"
+                        + param + "' is encountered!\n");
     }
+    RLOG(DEBUG) << msg << "\n";
+    return *this;
+  }
+   
+  template <class MATRIX>
+  KMeans<T>& 
+  fit(MATRIX& mat,
+    const dvector<T>& label = dvector<T>()) { // ignored
+    if(use_shrink) { 
+      centroid = frovedis::shrink::kmeans(mat, k, max_iter, eps, seed, n_init,
+                                          n_iter, inertia, labels);
+    }
+    else {
+      centroid = frovedis::kmeans(mat, k, max_iter, eps, seed, n_init, 
+                                  n_iter, inertia, labels);
+    }
+    is_fitted = true;
+    return *this;           
+  }
 
-   KMeans<T>& 
-   set_k(int k) {
-     std::string msg = "expected a positive k; received: " + STR(k) + "\n";
-     require(k > 0, msg);
-     this->k = k;
-     return *this;
-   }
-    
-   KMeans<T>&
-   set_max_iter(int max_iter) {
-     std::string msg = "expected a positive max_iter; received: " + STR(max_iter) + "\n";
-     require(max_iter > 0, msg);
-     this->max_iter = max_iter;
-     return *this;
-   }
+  template <class MATRIX>
+  std::vector<int>
+  fit_predict(MATRIX& mat,
+    const dvector<T>& label = dvector<T>()) { // ignored
+    fit(mat, label);
+    return labels;
+  }
 
-   KMeans<T>& 
-   set_eps(double eps) { // eps >= 0.0 && eps <= 1.0
-     std::string msg = "expected eps within the range of 0.0 to 1.0; received: " + STR(eps) + "\n";
-     require(eps >= 0.0 && eps <= 1.0, msg);
-     this->eps = eps;
-     return *this;
-   }
-
-   KMeans<T>& 
-   set_seed(long seed) { 
-     std::string msg = "expected a zero or positive value for seed; received: " + STR(seed) + "\n";
-     require(seed >= 0, msg);
-     this->seed = seed;
-     return *this;
-   }
-
-   KMeans<T>&
-   set_use_shrink(bool shrink) {
-     this->use_shrink = shrink;
-     return *this;
-   }
-
-   KMeans<T>& 
-   set_params(std::vector<std::pair<std::string, param_t>>& data) {
-     std::string msg = "";
-     for (auto& e: data) {
-       auto param = e.first;
-       auto val = e.second;
-       if(param == "k") {
-         set_k(val.get<int>());
-         msg += "k: " + val.tt + "; ";
-       }
-       else if(param == "max_iter") {
-         set_max_iter(val.get<int>());
-         msg += "max_iter: " + val.tt + "; ";
-       }
-       else if(param == "eps") {
-         set_eps(val.get<double>());
-         msg += "eps: " + val.tt + "; ";
-       }
-       else if(param == "seed") {
-         set_seed(val.get<long>());
-         msg += "seed: " + val.tt + "; ";
-       }
-       else if(param == "use_shrink") {
-         set_use_shrink(val.get<bool>());
-         msg += "use_shrink: " + val.tt + "; ";
-       }
-       else REPORT_ERROR(USER_ERROR, "[KMeans] Unknown parameter: '"
-                         + param + "' is encountered!\n");
-     }
-     RLOG(DEBUG) << msg << "\n";
-     return *this;
-   }
-    
-   template <class MATRIX>
-   std::vector<int> 
-   label_inertia(MATRIX& mat, T& inertia, bool need_inertia) {
-     auto l_inertia = make_node_local_allocate<T>();
-     auto lbl =  mat.data.map(invoke_kmeans_assign_cluster
-                              <typename MATRIX::local_mat_type, T>,
-		              frovedis::broadcast(centroid), 
-                              l_inertia,
-                              frovedis::broadcast(need_inertia))
-	                 .template moveto_dvector<int>()
-                         .gather();
-     inertia = l_inertia.reduce(add<T>);
-     return lbl;
-   }
-
-   template <class MATRIX>
-   KMeans<T>& 
-   fit(MATRIX& mat,
-     const dvector<T>& label = dvector<T>()) { // ignored
-     n_iter = 0;
-     if(use_shrink) 
-       centroid = frovedis::shrink::kmeans(mat, k, max_iter, eps, seed, n_iter);
-     else 
-       centroid = frovedis::kmeans(mat, k, max_iter, eps, seed, n_iter);
-     is_fitted = true;
-     labels = label_inertia(mat, inertia, true);
-     return *this;           
-   }
-
-   template <class MATRIX>
-   std::vector<int>
-   fit_predict(MATRIX& mat,
-     const dvector<T>& label = dvector<T>()) { // ignored
-     fit(mat, label);
-     return labels;
-   }
-
-   template <class MATRIX>
-   std::vector<int> 
-   predict(MATRIX& mat) {
-     if(!is_fitted) REPORT_ERROR(USER_ERROR,
-		    "[KMeans] prediction is called before fit\n");
-     T inertia_score = 0.0;
-     return label_inertia(mat, inertia_score, false);
-   }
+  template <class MATRIX>
+  std::vector<int> 
+  predict(MATRIX& mat) {
+    require(is_fitted, "[KMeans] predict(): is called before fit\n");
+    return parallel_kmeans_assign_cluster(mat, centroid);
+  }
   
-   template <class MATRIX>
-   float score(MATRIX& mat, 
-     const dvector<T>& label = dvector<T>()) { // ignored
-     if(!is_fitted) REPORT_ERROR(USER_ERROR,
-         	   "[KMeans] score is called before fit\n");
-     T inertia_score = 0.0;
-     label_inertia(mat, inertia_score, true);
-     return static_cast<float>(inertia_score);
-   }
+  template <class MATRIX>
+  float score(MATRIX& mat, 
+    const dvector<T>& label = dvector<T>()) { // ignored
+    require(is_fitted, "[KMeans] score(): is called before fit\n");
+    float inertia_score = 0.0;
+    parallel_kmeans_assign_cluster(mat, centroid, inertia_score);
+    return -1.0f * inertia_score;
+  }
+
+  template <class MATRIX>
+  rowmajor_matrix<T>
+  transform(MATRIX& mat) {
+    require(is_fitted, "[KMeans] transform(): is called before fit\n");
+    return parallel_kmeans_transform(mat, centroid);
+  }
+
+  template <class MATRIX>
+  rowmajor_matrix<T>
+  fit_transform(MATRIX& mat,
+    const dvector<T>& label = dvector<T>()) { // ignored
+    fit(mat, label);
+    return parallel_kmeans_transform(mat, centroid);
+  }
   
-  rowmajor_matrix_local<T> cluster_centers_() { return centroid; } 
-
-  std::vector<int> labels_() { return labels; }   
-
-  T inertia_() { return inertia; }
-
-  int n_iter_() { return n_iter; }
-
   void debug_print(size_t limit = 0) { 
     std::cout << "centroid: \n";
-    centroid.debug_print(limit); 
+    centroid.transpose().debug_print(limit); 
   }
 
   void savebinary(const std::string& fname) { 
@@ -196,21 +190,48 @@ namespace frovedis {
     is_fitted = true;
     // TODO: load other metadata
   }
-
+  
   typedef T value_type;
+  typedef int predict_value_type;
   typedef rowmajor_matrix_local<T> model_type;
+
+  // attributes
+  rowmajor_matrix_local<T> cluster_centers_() { 
+    require(is_fitted, "cluster_centers_: attribute can be obtained after fit!\n");
+    return centroid; 
+  } 
+
+  std::vector<int> labels_() { 
+    require(is_fitted, "labels_: attribute can be obtained after fit!\n");
+    return labels; 
+  }   
+
+  float inertia_() { 
+    require(is_fitted, "inertia_: attribute can be obtained after fit!\n");
+    return inertia; 
+  }
+
+  int n_iter_() { 
+    require(is_fitted, "n_iter_: attribute can be obtained after fit!\n");
+    return n_iter;
+   }
+
+  int n_clusters_() {
+    require(is_fitted, "n_clusters_: attribute can be obtained after fit!\n");
+    return centroid.local_num_col;
+  }
 
   private:
     int k;
-    int max_iter;
+    int n_init, max_iter;
     double eps;
     long seed;
     bool is_fitted, use_shrink;
     std::vector<int> labels;
-    T inertia;
+    float inertia;
     int n_iter;
     rowmajor_matrix_local<T> centroid;
-    SERIALIZE(k, max_iter, eps, seed, is_fitted, use_shrink, 
+    SERIALIZE(k, n_init, max_iter, eps, seed, is_fitted, use_shrink,
               labels, inertia, n_iter, centroid);
 };
 
