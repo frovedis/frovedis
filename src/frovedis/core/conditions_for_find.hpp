c4175ee5742f48c4a49d788973768460994269a4
Takuya Araki <takuy_araki@nec.com>
2021-02-12 18:16:49 +0900

    supported vector find conditions for gt/ge, lt/le, eq/neq cases, modified vector_take, vector_bincount to type cast in-place if needed; added few more vector find conditions as per need (NTI)

--- src/frovedis/core/conditions_for_find.hpp
+++ src/frovedis/core/conditions_for_find.hpp
@@ -63,6 +63,85 @@ struct is_binary {
   int operator()(T a) const {return (a == 0 || a == 1);}
 };
 
+template <class T>
+struct is_gt {
+  T threshold;
+  is_gt() {}
+  is_gt(T thr): threshold(thr) {}
+  int operator()(T a) const {return (a > threshold);}
+};
+
+template <class T>
+struct is_gt_and_neq {
+  T threshold, threshold2;
+  is_gt_and_neq() {}
+  is_gt_and_neq(T thr, T thr2): threshold(thr), threshold2(thr2) {}
+  int operator()(T a) const {return (a > threshold && a != threshold2);}
+};
+
+template <class T>
+struct is_ge {
+  T threshold;
+  is_ge() {}
+  is_ge(T thr): threshold(thr) {}
+  int operator()(T a) const {return (a >= threshold);}
+};
+
+template <class T>
+struct is_ge_and_neq {
+  T threshold, threshold2;
+  is_ge_and_neq() {}
+  is_ge_and_neq(T thr, T thr2): threshold(thr), threshold2(thr2) {}
+  int operator()(T a) const {return (a >= threshold && a != threshold2);}
+};
+
+template <class T>
+struct is_lt {
+  T threshold;
+  is_lt() {}
+  is_lt(T thr): threshold(thr) {}
+  int operator()(T a) const {return (a < threshold);}
+};
+
+template <class T>
+struct is_lt_and_neq {
+  T threshold, threshold2;
+  is_lt_and_neq() {}
+  is_lt_and_neq(T thr, T thr2): threshold(thr), threshold2(thr2) {}
+  int operator()(T a) const {return (a < threshold && a != threshold2);}
+};
+
+template <class T>
+struct is_le {
+  T threshold;
+  is_le() {}
+  is_le(T thr): threshold(thr) {}
+  int operator()(T a) const {return (a <= threshold);}
+};
+
+template <class T>
+struct is_le_and_neq {
+  T threshold, threshold2;
+  is_le_and_neq() {}
+  is_le_and_neq(T thr, T thr2): threshold(thr), threshold2(thr2) {}
+  int operator()(T a) const {return (a <= threshold && a != threshold2);}
+};
+
+template <class T>
+struct is_eq {
+  T threshold;
+  is_eq() {}
+  is_eq(T thr): threshold(thr) {}
+  int operator()(T a) const {return (a == threshold);}
+};
+
+template <class T>
+struct is_neq {
+  T threshold;
+  is_neq() {}
+  is_neq(T thr): threshold(thr) {}
+  int operator()(T a) const {return (a != threshold);}
+};
 
 }
 
