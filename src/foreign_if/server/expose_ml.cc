7dbe13185b8448e2f495082501b2a25b440ae636
Takuya Araki <takuy_araki@nec.com>
2021-02-12 17:15:25 +0900

    added n_init, cluster_centers, fit_transform, transform etc. for kmeans, etc. in server code (NTI)

--- src/foreign_if/server/expose_ml.cc
+++ src/foreign_if/server/expose_ml.cc
@@ -137,13 +137,20 @@ void expose_frovedis_ml_functions() {
   expose((frovedis_ridge_lbfgs<DT2,S_MAT25>)); // python case
   // (8) kmeans clustering
   // as for dense input, kmeans supports only rowmajor_matrix<T> for dense data
-  expose((frovedis_kmeans<DT1,R_MAT1>));  // both spark and python case
-  expose((frovedis_kmeans<DT2,R_MAT2>));  // python case
-  expose((frovedis_kmeans<DT1,S_MAT1>));  // spark case
-  expose((frovedis_kmeans<DT1,S_MAT14>)); // python case
-  expose((frovedis_kmeans<DT1,S_MAT15>)); // python case
-  expose((frovedis_kmeans<DT2,S_MAT24>)); // python case
-  expose((frovedis_kmeans<DT2,S_MAT25>)); // python case
+  expose((frovedis_kmeans_fit<DT1,R_MAT1>));  // both spark and python case
+  expose((frovedis_kmeans_fit<DT2,R_MAT2>));  // python case
+  expose((frovedis_kmeans_fit<DT1,S_MAT1>));  // spark case
+  expose((frovedis_kmeans_fit<DT1,S_MAT14>)); // python case
+  expose((frovedis_kmeans_fit<DT1,S_MAT15>)); // python case
+  expose((frovedis_kmeans_fit<DT2,S_MAT24>)); // python case
+  expose((frovedis_kmeans_fit<DT2,S_MAT25>)); // python case
+  expose((frovedis_kmeans_fit_transform<DT1,R_MAT1>));  // both spark and python case
+  expose((frovedis_kmeans_fit_transform<DT2,R_MAT2>));  // python case
+  expose((frovedis_kmeans_fit_transform<DT1,S_MAT1>));  // spark case
+  expose((frovedis_kmeans_fit_transform<DT1,S_MAT14>)); // python case
+  expose((frovedis_kmeans_fit_transform<DT1,S_MAT15>)); // python case
+  expose((frovedis_kmeans_fit_transform<DT2,S_MAT24>)); // python case
+  expose((frovedis_kmeans_fit_transform<DT2,S_MAT25>)); // python case
   // (9) matrix factorization using als
   expose((frovedis_mf_als<DT1,S_MAT1>));  // spark case
   expose((frovedis_mf_als<DT1,S_MAT14>)); // python case
@@ -162,8 +169,7 @@ void expose_frovedis_ml_functions() {
   //expose(get_frovedis_fpm<std::string>);
   expose(get_frovedis_fpm<int>);
   // (12) hierarchical clustering
-  expose((frovedis_aca2<DT1,R_MAT1>)); // spark case (only fit)
-  expose((frovedis_aca<DT1,R_MAT1>));  // python case (fit-predict)
+  expose((frovedis_aca<DT1,R_MAT1>));  // python+spark case (fit-predict)
   expose((frovedis_aca<DT2,R_MAT2>));  // python case (fit-predict)
   // (13) spectral clustering
   expose((frovedis_sca<DT1,R_MAT1>));
