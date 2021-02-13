97878639802c087cb241b393d723ee06d0251793
Takuya Araki <takuy_araki@nec.com>
2021-02-13 15:10:07 +0900

    added missing attributes in DBSCAN (NTI)

--- src/frovedis/ml/clustering/dbscan.hpp
+++ src/frovedis/ml/clustering/dbscan.hpp
@@ -5,84 +5,117 @@
 #include "frovedis/ml/clustering/dbscan_impl.hpp"
 
 namespace frovedis {
-  
-  struct dbscan { 
-    dbscan(double eps = 0.5, int min_pts = 5) : 
-      eps(eps), min_pts(min_pts), is_fitted(false) {}
 
-    dbscan& 
-    set_eps(double eps) { 
-      std::string msg = "expected eps within the range of 0.0 to 1.0; received: " + 
-                         STR(eps) + "\n";
-      require(eps >= 0.0 && eps <= 1.0, msg);
-      this->eps = eps;
-      return *this;
-    }
+template <typename T>  
+struct dbscan { 
+  dbscan(double eps = 0.5, int min_pts = 5) : 
+    eps(eps), min_pts(min_pts), is_fitted(false) {}
 
-    dbscan&
-    set_min_pts(int min_pts) {
-      std::string msg = "min_pts should be a positive value \n";  
-      require(min_pts > 0, msg);  
-      this->min_pts = min_pts;
-      return *this;
-    }
+  dbscan<T>& 
+  set_eps(double eps) { 
+    std::string msg = "expected eps within the range of 0.0 to 1.0; received: " + 
+                       STR(eps) + "\n";
+    require(eps >= 0.0 && eps <= 1.0, msg);
+    this->eps = eps;
+    return *this;
+  }
 
-    dbscan& 
-    set_params(std::vector<std::pair<std::string, param_t>>& data) {
-      std::string msg = "";
-      for (auto& e: data) {
-        auto param = e.first;
-        auto val = e.second;
-        if(param == "eps") {
-          set_eps(val.get<double>());
-          msg += "eps: " + val.tt + "; ";
-        }
-        else if(param == "min_pts") {
-          set_min_pts(val.get<int>());
-          msg += "seed: " + val.tt + "; ";
-        }
-        else REPORT_ERROR(USER_ERROR, "[DBSCAN] Unknown parameter: '"
-                          + param + "' is encountered!\n");
+  dbscan<T>&
+  set_min_pts(int min_pts) {
+    std::string msg = "min_pts should be a positive value \n";  
+    require(min_pts > 0, msg);  
+    this->min_pts = min_pts;
+    return *this;
+  }
+
+  dbscan<T>& 
+  set_params(std::vector<std::pair<std::string, param_t>>& data) {
+    std::string msg = "";
+    for (auto& e: data) {
+      auto param = e.first;
+      auto val = e.second;
+      if(param == "eps") {
+        set_eps(val.get<double>());
+        msg += "eps: " + val.tt + "; ";
       }
-      RLOG(DEBUG) << msg << "\n";
-      return *this;
+      else if(param == "min_pts") {
+        set_min_pts(val.get<int>());
+        msg += "seed: " + val.tt + "; ";
+      }
+      else REPORT_ERROR(USER_ERROR, "[DBSCAN] Unknown parameter: '"
+                        + param + "' is encountered!\n");
     }
+    RLOG(DEBUG) << msg << "\n";
+    return *this;
+  }
 
-    template <class MATRIX>    
-    dbscan& fit(MATRIX& mat, 
-            const dvector<int>& label = dvector<int>()) { //ignored
-      est = dbscan_impl(eps, min_pts);
-      est.fit(mat);
-      is_fitted = true;       
-      return *this;
-    }
+  template <class MATRIX>    
+  dbscan<T>& 
+  fit(MATRIX& mat, 
+      const dvector<int>& label = dvector<int>()) { //ignored
+    est = dbscan_impl<T>(eps, min_pts);
+    est.fit(mat);
+    is_fitted = true;       
+    return *this;
+  }
 
-    template <class MATRIX>    
-    std::vector<int> fit_predict(MATRIX& mat, 
-            const dvector<int>& label = dvector<int>()) { //ignored
-      fit(mat, label);     
-      return est.labels();
-    }   
+  template <class MATRIX>    
+  dbscan<T>& 
+  fit(MATRIX& mat,
+      std::vector<T>& sample_weight, 
+      const dvector<int>& label = dvector<int>()) { //ignored
+    est = dbscan_impl<T>(eps, min_pts);
+    est.fit(mat, sample_weight);
+    is_fitted = true;       
+    return *this;
+  }
 
-    template <class MATRIX>    
-    float score(MATRIX& mat, dvector<int>& true_label) {
-      auto pred_label = fit_predict(mat);
-      return homogeneity_score(true_label.gather(), pred_label);
-    }
+  template <class MATRIX>    
+  std::vector<int> 
+  fit_predict(MATRIX& mat, 
+             const dvector<int>& label = dvector<int>()) { //ignored
+    fit(mat, label);     
+    return est.labels();
+  }   
+
+  template <class MATRIX>    
+  std::vector<int> 
+  fit_predict(MATRIX& mat, 
+              std::vector<T>& sample_weight,
+              const dvector<int>& label = dvector<int>()) { //ignored
+    fit(mat, sample_weight, label);     
+    return est.labels();
+  }   
+
+  template <class MATRIX>    
+  float score(MATRIX& mat, dvector<int>& true_label) {
+    auto pred_label = fit_predict(mat);
+    return homogeneity_score(true_label.gather(), pred_label);
+  }
     
-    std::vector<int> labels() {
-      require(is_fitted, "labels_() is called before fit()");
-      return est.labels(); 
-    }
+  std::vector<int> labels() {
+    require(is_fitted, "labels_() is called before fit()");
+    return est.labels(); 
+  }
 
-    private:
-      dbscan_impl est;
-      double eps;
-      int min_pts;
-      bool is_fitted;
-      SERIALIZE(est, eps, min_pts, is_fitted);
-      typedef int predict_type;
-  };
+  std::vector<size_t> core_sample_indices_() {
+    require(is_fitted, "components__() is called before fit()");
+    return est.core_sample_indices_(); 
+  }
+
+  rowmajor_matrix<T> components_() { 
+    require(is_fitted, "components_() is called before fit()");
+    return est.components_();
+  }
+
+  private:
+    dbscan_impl<T> est;
+    double eps;
+    int min_pts;
+    bool is_fitted;
+    SERIALIZE(est, eps, min_pts, is_fitted);
+    typedef int predict_type;
+};
+
 }
-
 #endif
--- src/frovedis/ml/clustering/dbscan_impl.hpp
+++ src/frovedis/ml/clustering/dbscan_impl.hpp
@@ -8,21 +8,37 @@
 namespace frovedis {
 
 //  Originally written by Tadayoshi Hara
+template <typename T>
 class dbscan_impl {
 public:
-  template<typename T>
   void fit(rowmajor_matrix<T>& vector_set) {
     num_row = vector_set.num_row;
     d_graph = calc_graph(vector_set, eps);
-    d_core_flgs = calc_core_flgs(min_pts);
+    d_core_flgs = calc_core_flgs(min_pts, (T) 1.0);
+    set_components(vector_set);
+    d_core_graph = calc_core_graph();
+    cluster();
+  }
+
+  void fit(rowmajor_matrix<T>& vector_set, std::vector<T>& sample_weight) {
+    num_row = vector_set.num_row;
+    require(sample_weight.size() == num_row,
+    "sample_weight size does not match with number of samples in input data");
+    d_graph = calc_graph(vector_set, eps);
+    if(vector_is_uniform(sample_weight))
+      d_core_flgs = calc_core_flgs(min_pts, sample_weight[0]);
+    else
+      d_core_flgs = calc_core_flgs(min_pts, sample_weight);
+    set_components(vector_set);
     d_core_graph = calc_core_graph();
     cluster();
   }
   std::vector<int> labels() {return clustered_labels;}
   dbscan_impl(double eps=0.5, int min_pts=5) : eps(eps), min_pts(min_pts) {}
+  std::vector<size_t> core_sample_indices_() {return core_sample_indices;}
+  rowmajor_matrix<T> components_() { return components; }
 
 private:
-  template<typename T>
   node_local<crs_matrix_local<int>> calc_graph(rowmajor_matrix<T>& vector_set,
                                                double eps) {
     auto g_vector_set = get_global_data(vector_set);
@@ -31,19 +47,46 @@ private:
     auto ret = construct_connectivity_graph<int>(dist_m, eps);
     return ret.data;
   }
-
-  dvector<int> calc_core_flgs(int min_pts) {
-    auto min_pts_l = make_node_local_broadcast(min_pts);
+  
+  dvector<int> calc_core_flgs(int min_pts, T weight) {
     auto is_bigger_than_min_pts = +[] (crs_matrix_local<int>& graph,
-                                       int& min_pts) {
-      auto core_flgs = std::vector<int>(graph.off.size() - 1, 0);
-      for(int i=1;i<graph.off.size();i++) {
-        core_flgs[i - 1] = (graph.off[i] - graph.off[i-1]) > (min_pts - 1);
+                                       int min_pts, T weight) {
+      auto nrow = graph.local_num_row; 
+      auto core_flgs = vector_zeros<int>(nrow);
+      auto offp = graph.off.data();
+      auto flgsp = core_flgs.data();
+      for(size_t i = 1; i <= nrow; i++) {
+        flgsp[i-1] = (offp[i] - offp[i-1]) * weight >= min_pts;
+      }
+     
+      return core_flgs;
+    };
+    return d_graph.map(is_bigger_than_min_pts, broadcast(min_pts), 
+                       broadcast(weight)).
+                       template moveto_dvector<int>();
+  };
+
+  dvector<int> calc_core_flgs(int min_pts, std::vector<T>& sample_weight) {
+    auto is_bigger_than_min_pts = +[] (crs_matrix_local<int>& graph,
+                                       int min_pts, 
+                                       std::vector<T>& sample_weight) {
+      auto nrow = graph.local_num_row; 
+      auto core_flgs = vector_zeros<int>(nrow);
+      auto flgsp = core_flgs.data();
+      auto swp = sample_weight.data();
+      auto midxp = graph.idx.data();
+      auto moffp = graph.off.data();
+
+      for(size_t i = 1; i <= nrow; i++) {
+        T pts = 0.0;
+        for(size_t j = moffp[i-1]; j < moffp[i]; ++j) pts += swp[midxp[j]];
+        flgsp[i - 1] = (pts >= min_pts);
       }
       return core_flgs;
     };
-    return d_graph.map(is_bigger_than_min_pts, min_pts_l).
-      template moveto_dvector<int>();
+
+    return d_graph.map(is_bigger_than_min_pts, broadcast(min_pts), 
+                       broadcast(sample_weight)).template moveto_dvector<int>();
   };
 
   node_local<crs_matrix_local<int>> calc_core_graph() {
@@ -186,13 +229,30 @@ private:
     other_labeling();
   };
 
-  int num_row = 0;
+  void set_components(rowmajor_matrix<T>& vector_set) {
+    auto rowids = d_core_flgs.viewas_node_local().map(vector_find_one<int>);
+    components = extract_rows(vector_set, rowids);
+    auto myst = get_start_indices(vector_set);
+
+    auto convert_local_to_global = +[](std::vector<size_t>& vec, size_t myst) {
+      auto vp = vec.data();
+      auto vsz = vec.size();
+      for(size_t i = 0; i < vsz; ++i) vp[i] += myst;
+    };
+
+    rowids.mapv(convert_local_to_global, myst);
+    core_sample_indices = rowids.moveto_dvector<size_t>().gather();
+  }
+
+  size_t num_row = 0;
   node_local<crs_matrix_local<int>> d_graph;
   dvector<int> d_core_flgs;
   node_local<crs_matrix_local<int>> d_core_graph;
   std::vector<int> clustered_labels;
   double eps;
   int min_pts;
+  std::vector<size_t> core_sample_indices;
+  rowmajor_matrix<T> components;
 };
 }
 
