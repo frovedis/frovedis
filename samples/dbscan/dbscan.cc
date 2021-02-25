0c2843fde495a0570fe1eeb335aabf380ca86098
Takuya Araki <takuy_araki@nec.com>
2021-02-25 11:19:37 +0900

    corrected dbscan sample cpp code as per latest library changes (NTI)

--- samples/dbscan/dbscan.cc
+++ samples/dbscan/dbscan.cc
@@ -9,26 +9,27 @@ using namespace boost;
 using namespace frovedis;
 using namespace std;
 
+template <class T>
 void do_dbscan(const string& input, const string& output,
                double eps, int min_pts, bool binary) {
   if (binary) {
     time_spent t(DEBUG);
-    auto m = frovedis::make_rowmajor_matrix_loadbinary<double>(input);
+    auto m = frovedis::make_rowmajor_matrix_loadbinary<T>(input);
     t.show("load matrix data: ");
-    auto dbscan = frovedis::dbscan(eps, min_pts);
-    dbscan.fit(m);
+    auto est = frovedis::dbscan<T>(eps, min_pts);
+    est.fit(m);
     t.show("total excution time: ");
-    auto labels = dbscan.labels();
+    auto labels = est.labels();
     make_dvector_scatter(labels).savebinary(output);
     t.show("save labels data: ");
   } else {
     time_spent t(DEBUG);
-    auto m = frovedis::make_rowmajor_matrix_load<double>(input);
+    auto m = frovedis::make_rowmajor_matrix_load<T>(input);
     t.show("load matrix data: ");
-    auto dbscan = frovedis::dbscan(eps, min_pts);
-    dbscan.fit(m);
+    auto est = frovedis::dbscan<T>(eps, min_pts);
+    est.fit(m);
     t.show("total excution time: ");
-    auto labels = dbscan.labels();
+    auto labels = est.labels();
     make_dvector_scatter(labels).saveline(output);
     t.show("save labels data: ");
   }
@@ -43,8 +44,10 @@ int main(int argc, char* argv[]) {
     ("help,h", "print help")
     ("input,i", value<string>(), "input matrix")
     ("output,o", value<string>(), "output")
-    ("eps,e", value<double>(), "distance that can be determined to be reachable")
-    ("min_pts,m", value<int>(), "minimum number of core")
+    ("eps,e", value<double>(), "distance that can be determined to be reachable [default: 0.5]")
+    ("min_pts,m", value<int>(), "minimum number of core [default: 5]")
+    ("float,f", "for float type input")
+    ("double,d","for double type input(default input type is double)")
     ("verbose", "set loglevel to DEBUG")
     ("binary,b", "use binary input/output");
 
@@ -54,8 +57,8 @@ int main(int argc, char* argv[]) {
   notify(argmap);
 
   string input, output;
-  double eps;
-  int min_pts;
+  double eps = 0.5;
+  int min_pts = 5;
   bool binary = false;
   
   if(argmap.count("help")){
@@ -81,19 +84,11 @@ int main(int argc, char* argv[]) {
 
   if(argmap.count("eps")){
     eps = argmap["eps"].as<double>();
-  } else {
-    cerr << "eps is not specified" << endl;
-    cerr << opt << endl;
-    exit(1);
-  }
+  } 
 
   if(argmap.count("min_pts")){
     min_pts = argmap["min_pts"].as<int>();
-  } else {
-    cerr << "min_pts is not specified" << endl;
-    cerr << opt << endl;
-    exit(1);
-  }
+  } 
 
   if(argmap.count("binary")){
     binary = true;
@@ -103,5 +98,8 @@ int main(int argc, char* argv[]) {
     set_loglevel(DEBUG);
   }
 
-  do_dbscan(input, output, eps, min_pts, binary);
+  if(argmap.count("float")) 
+    do_dbscan<float>(input, output, eps, min_pts, binary);
+  else
+    do_dbscan<double>(input, output, eps, min_pts, binary);
 }
