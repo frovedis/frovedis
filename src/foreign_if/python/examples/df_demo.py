6073c18ebe0733f2c0aaebb60e9c3d43d81f0c6d
Takuya Araki <takuy_araki@nec.com>
2021-02-25 11:20:33 +0900

    added red_csv python dataframe demo, etc. (NTI)

--- src/foreign_if/python/examples/df_demo.py
+++ src/foreign_if/python/examples/df_demo.py
@@ -17,7 +17,8 @@ FrovedisServer.initialize(argvs[1])
 peopleDF = {
             'Ename' : ['Michael', 'Andy', 'Tanaka', 'Raul', 'Yuta'], 
             'Age' : [29, 30, 27, 19, 31],
-            'Country' : ['USA', 'England', 'Japan', 'France', 'Japan']
+            'Country' : ['USA', 'England', 'Japan', 'France', 'Japan'],
+            'isMale': [False, False, False, False, True]
            }
 
 countryDF = {
@@ -50,7 +51,7 @@ fdf1.sort_values(["Country", "Age"], ascending=[0,1]).show() # multiple column
 # groupby demo
 fdf1.groupby('Country')['Country'].show()
  
-fdf1.groupby('Country').agg({'Age': ['max','min','mean'], 
+fdf1.groupby('Country').agg({'Age': ['max','min','mean'],
                              'Ename': ['count']}).show()
 
 # merge demo
@@ -77,10 +78,10 @@ fdf3.show()
 
 # join after column renaming
 fdf1.merge(fdf3, left_on="Country", right_on="Cname").show() # with defaults
-fdf1.merge(fdf3, left_on="Country", right_on="Cname", how='outer', join_type='hash').show()
+fdf1.merge(fdf3, left_on="Country", right_on="Cname", how='left', join_type='hash').show()
 # operation chaining: join -> sort -> select -> show
 fdf1.merge(fdf3, left_on="Country", 
-           right_on="Cname", how='outer', join_type='hash').sort("Age")[["Age", "Ename", "Country"]].show()
+           right_on="Cname", how='left', join_type='hash').sort("Age")[["Age", "Ename", "Country"]].show()
 fdf3.release()
 
 # merging with panda dataframe
@@ -94,31 +95,21 @@ print(joined.to_panda_dataframe()); print("\n")
 joined.release()
 
 # miscellaneous
-print ("all count: ", fdf1.count()); print # all column counts
-print ("min(age): ", fdf1.min("Age")); print("\n")
-print ("max(age): ", fdf1.max("Age")); print("\n")
-print ("sum(age): ", fdf1.sum("Age")); print("\n")
-print ("avg(age): ", fdf1.avg("Age")); print("\n")
-print ("std(age): ", fdf1.std("Age")); print("\n")
-print ("count(age): ", fdf1.count("Age")); print("\n")
-print(fdf1.describe()); print("\n")
+# all column count, sum, max, mkin for all columns
+print ("all count: ", fdf1.agg(["count", "sum", "max", "min"])) 
+# column specific agg operations 
+print ("std(Age): ", fdf1.agg({"Age": ["std"]})); print("\n")
+print ("count(Country): ", fdf1.agg({"Country": ["count"]})) ; print("\n")
+print ("sum(isMale): ", fdf1.agg({"isMale": ["sum"]})) ;  print("\n")
 
-# describe demo
-data = {'one': [10, 12, 13, 15],
-        'two': [10.23, 12.20, 34.90, 100.12],
-        'three': ['F', 'F', 'D', 'A'],
-        'four': [0, 0, 1, 2]
-       }
-
-pdf = pd.DataFrame(data)
-print(pdf.describe()); print("\n")
-
-df = fdf.DataFrame(pdf)
-print(df.describe()); print("\n") # prints count, mean, std, sum, min, max
-
-# matrix conversion demo
+# read_csv demo
+df = fdf.read_csv("./input/numbers.csv", names=['one', 'two', 'three', 'four'])
 df.show()
 
+# describing all statictics for numeric columns
+print(df.describe()) # prints count, mean, std, sum, min, max
+
+# matrix conversion demo
 row_mat = df.to_frovedis_rowmajor_matrix(['one', 'two'], dtype=np.float64)
 row_mat.debug_print()
 
