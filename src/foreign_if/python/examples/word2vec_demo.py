be53f1e37b20e11ab77d2edb1cb643ede5408a96
Takuya Araki <takuy_araki@nec.com>
2021-03-03 09:07:19 +0900

    Added frovedis word2vec python wrapper code changes including gensim word2vec like similarity usecase (NTI)

--- src/foreign_if/python/examples/word2vec_demo.py
+++ src/foreign_if/python/examples/word2vec_demo.py
@@ -2,7 +2,7 @@
 
 import sys
 from frovedis.exrpc.server import FrovedisServer
-from frovedis.mllib.feature import Word2Vector
+from frovedis.mllib import Word2Vec
 
 # initializing the Frovedis server
 argvs = sys.argv
@@ -11,22 +11,41 @@ if (argc < 2):
     print ('Please give frovedis_server calling command as the first argument \n(e.g. "mpirun -np 2 -x /opt/nec/nosupport/frovedis/ve/bin/frovedis_server")')
     quit()
 
-textfile = "./input/text8-10k"
-encode = "./out/text_encode.bin"
-vocab = "./out/text_vocab.txt"
-count = "./out/text_count.bin"
-
-weight = "./out/text_weight.bin"
-model = "./out/text_model.txt"
-
-# x86 task
-wv = Word2Vector(minCount=5)
-wv.build_vocab_and_dump(textfile, encode, vocab, count) 
-
-# ve task
 FrovedisServer.initialize(argvs[1])
-wv.fit(encode, count, weight)
-FrovedisServer.shut_down()
 
-#x86 task
-wv.save(weight, vocab, model)
+data = [["cat", "say", "meow"], ["dog", "say", "woof"]]
+
+### Generate embeddings using build_vocab_and_dump and fit
+wv_model = Word2Vec(minCount=1)
+wv_model.build_vocab_and_dump(data) # Using corpus data
+wv_model.fit(data) # Using corpus data
+print(wv_model.wv)
+
+### You can also perform the same in one step as follows(using constructor)
+wv_model = Word2Vec(sentences=data, minCount=1) # Using corpus data
+print(wv_model.wv)
+
+### Get gensim like wv attribute for frovedis word2vec model
+try:
+    wv = wv_model.to_gensim_model()
+    print(wv)
+    print('Similarity of {} with {} is {}'.format(\
+           "cat", "dog", wv.similarity("cat", "dog")))
+except:
+    print("### gensim is not available... so skipping " +
+          "demo gensim related functionalities")
+
+### Generate document embeddings using fit-transform()
+textfile = "./input/text8-10k"
+model = "text_model.txt"
+wv_model = Word2Vec(minCount=1)
+embeddings = wv_model.build_vocab_and_dump(corpusFile=textfile).\
+                      fit_transform(corpusFile=textfile)
+print(embeddings)
+### Above embedding can be used for further downstream tasks like classification, clustering etc
+
+### Save word2vec embeddings
+wv_model.save(model, binary=False)
+#wv_model.save(model, binary=True) # In binary format
+
+FrovedisServer.shut_down()
