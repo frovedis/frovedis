68bfd80b087908f4308fad2b13c3961dad2c63bb
Takuya Araki <takuy_araki@nec.com>
2021-03-04 17:24:05 +0900

    supported newly added hyper-parameters in fp-growth python wrapper; modified load to return FIS count in loaded model; added retail data mining in fp-growth sample (NTI)

--- src/foreign_if/python/examples/fp_growth_demo.py
+++ src/foreign_if/python/examples/fp_growth_demo.py
@@ -19,15 +19,32 @@ data = [[1,4],
         [1],
         [1,2,5]]
 '''
-data = pd.read_csv("./input/groceries.csv").drop(['Item(s)'], axis=1)
-#print(data)
+
+def get_names(fname):
+    fp = open(fname, 'r')
+    line = fp.readline()
+    max_ncol = 0
+    while line:
+        ncol = len(line.split(" "))
+        if ncol > max_ncol:
+            max_ncol = ncol
+        line = fp.readline()
+    names = ["item_" + str(i) for i in range(max_ncol)]
+    return names
+
+data = pd.read_csv("./input/retail.dat", sep = " ", names = get_names("./input/retail.dat"))
+#data = pd.read_csv("./input/groceries.csv").drop(['Item(s)'], axis=1)
+
+fpm = FPGrowth(min_support = 0.05, tree_depth=10, 
+               compression_point=4, mem_opt_level=1)
 
 # groceries data contains non-numeric (string) items
-# encoding to numeric value is suggested for 
-# better performance, it is optional though...
-fpm = FPGrowth(minSupport = 0.05)#, encode_string_input = True)
+# encoding to numeric value is suggested by uncommenting below 
+# for better performance, it is optional though...
+#fpm.encode_string_input = True
 
 model = fpm.fit(data)
+print("frequent item count: %d" % (model.fis_count))
 print("frequent item sets: ")
 model.debug_print()
 
@@ -35,6 +52,7 @@ model.save("./out/FPModel") # saving the model
 model.release() # releasing model from memory after saving into files
 
 model.load("./out/FPModel") # loading the saved model for rule mining
+print("frequent item count in loaded model: %d" % (model.fis_count))
 model.generate_rules(0.05).debug_print()
 model.generate_rules(0.2).debug_print()
 model.generate_rules(0.9).debug_print()
