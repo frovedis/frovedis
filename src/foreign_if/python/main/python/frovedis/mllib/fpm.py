f2326161e4a14389308d461b3e4b972415aa156f
Takuya Araki <takuy_araki@nec.com>
2021-03-04 17:28:56 +0900

    supported newly added hyper-parameters in fp-growth python wrapper; modified load to return FIS count in loaded model; added retail data mining in fp-growth sample (NTI)

--- src/foreign_if/python/main/python/frovedis/mllib/fpm.py
+++ src/foreign_if/python/main/python/frovedis/mllib/fpm.py
@@ -16,13 +16,19 @@ import frovedis.dataframe as fpd
 
 class FPGrowth(object):
     """A python wrapper of Frovedis FP-Growth"""
-    def __init__(self, minSupport=0.3, verbose=0, 
+    def __init__(self, min_support=0.3, tree_depth=None,
+                 compression_point=4, mem_opt_level=0,
+                 verbose=0, 
                  encode_string_input=False):
-        self.minSupport = minSupport
+        self.min_support = min_support
+        self.tree_depth = tree_depth
+        self.compression_point = compression_point
+        self.mem_opt_level = mem_opt_level
         self.verbose = verbose
         self.encode_string_input = encode_string_input
         self.__mid = None
         self.__mkind = M_KIND.FPM
+        self.encode_logic = None
 
     def __encode_input(self, data):
         import itertools
@@ -72,12 +78,17 @@ class FPGrowth(object):
             item_list.append(item)
         return self.__convert_iterable(item_list)
 
-    def fit(self, data):
-        """
-        NAME: fit
-        """
-        if self.minSupport < 0:
-            raise ValueError("fit: negative minsupport factor is not allowed!")
+    def __check_input(self, data):
+        if self.min_support < 0 or self.min_support > 1:
+            raise ValueError("fit: min_support value must be within 0 to 1.\n")
+        if self.tree_depth is None:
+            self.tree_depth = np.iinfo(np.int32).max
+        if self.tree_depth < 1:
+            raise ValueError("fit: tree_depth value must be >= 1.\n")
+        if self.compression_point < 2:
+            raise ValueError("fit: compression_point value must be >= 2.\n")
+        if self.mem_opt_level != 0 and self.mem_opt_level != 1:
+            raise ValueError("fit: mem_opt_level value must be 0 or 1.\n")
         if isinstance(data, pd.DataFrame):
             f_df = self.__convert_pandas_df(data)
         elif isinstance(data, fpd.DataFrame):
@@ -87,11 +98,20 @@ class FPGrowth(object):
         else:
             raise ValueError("fit: only dataframe and iterable " + 
                              "inputs are supported!\n")
+        return f_df
+
+    def fit(self, data):
+        """
+        NAME: fit
+        """
+        f_df = self.__check_input(data)
         self.release()
         self.__mid = ModelID.get()
         (host, port) = FrovedisServer.getServerInstance()
-        rpclib.fpgrowth_trainer(host, port, f_df.get(), self.__mid, \
-            self.minSupport, self.verbose)
+        self.fis_count = rpclib.fpgrowth_trainer(host, port, \
+            f_df.get(), self.__mid, \
+            self.min_support, self.tree_depth, self.compression_point, \
+            self.mem_opt_level, self.verbose)
         excpt = rpclib.check_server_exception()
         if excpt["status"]:
             raise RuntimeError(excpt["info"])
@@ -116,8 +136,8 @@ class FPGrowth(object):
         self.release()
         self.__mid = ModelID.get()
         (host, port) = FrovedisServer.getServerInstance()
-        rpclib.load_frovedis_model(host, port, self.__mid, self.__mkind,
-                                   DTYPE.DOUBLE, fname.encode('ascii'))
+        self.fis_count = rpclib.load_fp_model(host, port, self.__mid, 
+                         self.__mkind, fname.encode('ascii'))
         excpt = rpclib.check_server_exception()
         if excpt["status"]:
             raise RuntimeError(excpt["info"])
@@ -146,6 +166,8 @@ class FPGrowth(object):
         if self.__mid is not None:
             GLM.release(self.__mid, self.__mkind, DTYPE.DOUBLE)
             self.__mid = None
+            self.encode_logic = None
+            self.fis_count = None
 
     def __del__(self):
         """
