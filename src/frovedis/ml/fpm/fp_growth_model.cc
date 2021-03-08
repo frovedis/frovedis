ee421ce815758591b1fd03dd029fa7493fa48f74
Takuya Araki <takuy_araki@nec.com>
2021-03-08 17:41:25 +0900

    added save/savebinary, load/loadbinary, count etc. for association_rule; kimproved memory issue during rule generation (NTI)

--- src/frovedis/ml/fpm/fp_growth_model.cc
+++ src/frovedis/ml/fpm/fp_growth_model.cc
@@ -85,10 +85,10 @@ namespace frovedis {
 
   void fp_growth_model::debug_print() {
     for (size_t i = 0; i < item.size(); ++i) {
-      std::cout << "---tree[" << i << "]---\n";
+      std::cout << "--- tree[" << i << "] ---\n";
       item[i].show(); 
       std::cout << std::endl;
-      std::cout << "---tree_info[" << i << "]---\n";
+      std::cout << "--- tree_info[" << i << "] ---\n";
       tree_info[i].show();
       std::cout << std::endl;
     }
@@ -168,28 +168,6 @@ namespace frovedis {
     *this = fp_growth_model(std::move(tree_item), std::move(tree_info));
   }
   
-  //pass-by-value to avoid changes in input dftable
-  dftable create_antacedent(dftable df, int j){ 
-      auto col_name = df.columns();
-      std::vector<std::string> cols_list;
-      df.rename(col_name[j],"consequent");
-      df.rename("count","union_count");
-      int k = 1;
-      for(size_t i = 0; i < col_name.size()-1; i++) {
-        if(i != j) {
-          auto new_name = "antacedent" + std::to_string(k++);
-          df.rename(col_name[i],new_name);
-          cols_list.push_back(new_name);
-        }
-      }
-      cols_list.push_back("consequent");
-      cols_list.push_back("union_count");
-#ifdef FP_DEBUG
-      show("ANT::Cols list: ", cols_list);
-#endif
-      return df.select(cols_list);
-  }
-  
   struct diff {
     double operator()(size_t a, size_t b) { 
       return static_cast<double>(a) / static_cast<double>(b);
@@ -197,87 +175,204 @@ namespace frovedis {
     SERIALIZE_NONE
   };
 
-  dftable calculate_confidence(dftable& pre, dftable& post, double con){
+  void calculate_confidence(dftable& pre, dftable& post,
+                            double conf, int depth_level,
+                            std::vector<dftable>& result) {
+    std::string count = "count";
+    std::string union_count = "union_count";
+    std::string consequent = "consequent";
+    std::string confidence = "confidence";
+    std::string antacedent = "antacedent";
     auto pre_col = pre.columns();
     auto post_col = post.columns();
-    auto col_list1 = post_col;
-    //will contain all antacedants, consequent, union_count and count
-    col_list1.push_back("count"); 
-    auto col_list2 = post_col;
-    col_list2.pop_back(); //popping union_counts
-    // will conatin all antacedants, consequent and confidence
-    col_list2.push_back("confidence"); 
-    auto iter = pre.num_col()-1;
+    auto iter = pre_col.size() - 1;
+    auto post_ncol = post_col.size();
     std::vector<std::string> opt_left(iter), opt_right(iter);
-    for (size_t i = 0; i < iter; ++i){
+    std::vector<std::string> targets1(post_ncol + 1), targets2(post_ncol);
+    for(size_t i = 0; i < iter; ++i) {
       opt_left[i] = pre_col[i];
-      opt_right[i] = post_col[i];
+      auto ant_name = antacedent + std::to_string(i+1);
+      opt_right[i] = targets1[i] = targets2[i] = ant_name;
     }
+    targets1[iter] = targets2[iter] = consequent; // ant1, ... antN, consequent
+    targets1[iter + 1] = union_count; 
+    targets1[iter + 2] = count;      // + union_count, count
+    targets2[iter + 1] = confidence; // + confidence
 #ifdef FP_DEBUG
-    show("pre-col: ", pre_col);
-    show("post-col: ", post_col);
-    show("col-list1: ", col_list1);
-    show("col-list2: ", col_list2);
     show("opt-left: ", opt_left);
     show("opt-right: ", opt_right);
-    std::cout << " ------ Pre ---" << std::endl;   pre.show();
-    std::cout << " ------ Post ---" << std::endl;  post.show();
+    show("select-targets1: ", targets1);
+    show("select-targets2: ", targets2);
 #endif
-    auto result = pre.bcast_join(post,multi_eq(opt_left, opt_right));
-    return result.select(col_list1)
-                 .calc<double,size_t,size_t>(std::string("confidence"),diff(),
-                                             std::string("union_count"),
-                                             std::string("count"))
-                 .filter(ge_im("confidence",con))
-                 .select(col_list2);
+
+    post.rename(post_col[post_ncol - 1], union_count); // count -> union_count
+    auto tmp_post_col = post.columns();
+    for(size_t i = 0; i < post_ncol - 1; ++i) {
+      if (i == 0) {
+        size_t k = 1;
+        for (size_t j = 0; j < post_ncol - 1; ++j) {
+          if (i == j) post.rename(tmp_post_col[j], consequent); 
+          else post.rename(tmp_post_col[j], antacedent + std::to_string(k++));
+        }
+      }
+      else { // swap name of ith column with i-1th column
+        auto t1 = tmp_post_col[i];
+        auto t2 = tmp_post_col[i - 1];
+        post.rename(t2, "tmp");
+        post.rename(t1, t2); // consequent
+        post.rename("tmp", t1);
+      }
+      tmp_post_col = post.columns(); // renamed columns
+#ifdef FP_DEBUG
+      show("pre-col: ", pre_col);
+      show("post-col: ", tmp_post_col);
+      std::cout << " ------ Pre ---" << std::endl;   pre.show();
+      std::cout << " ------ Post ---" << std::endl;  post.show();
+#endif
+      dftable rule;
+      if(post.num_row() > pre.num_row()) {
+        // std::cout << "join: " << post.num_row() << " x " << pre.num_row() << "\n";
+        rule = post.bcast_join(pre, multi_eq(opt_right, opt_left))
+                   .select(targets1); // ant1, ..., antN, consequent, union_count, count
+      }
+      else {
+        // std::cout << "join: " << pre.num_row() << " x " << post.num_row() << "\n";
+        rule = pre.bcast_join(post, multi_eq(opt_left, opt_right))
+                  .select(targets1); // ant1, ..., antN, consequent, union_count, count
+      }
+      rule = rule.calc<double,size_t,size_t>(confidence, diff(),
+                                             union_count, count)
+                 .filter(ge_im(confidence, conf))
+                 .select(targets2); // ant1, ..., antN, consequent, confidence
+#ifdef FP_DEBUG
+         std::cout << "--- [depth: " << depth_level 
+                   << "; iter: " << i << "] rule --- \n";
+         rule.show();
+#endif
+      if (rule.num_row()) result.push_back(std::move(rule));
+    }
+    for(size_t i = 0; i < post_ncol; ++i) {
+      post.rename(tmp_post_col[i], post_col[i]); // rename-back to original
+    }
   }
 
   association_rule
-  generate_association_rules(std::vector<dftable>& freq_itemsets,
-                             double con) {
-    std::vector<dftable> ass_rule;
-#ifdef FP_DEBUG
-    std::cout << "NUM FREQ ITEM SETS: " << freq_itemsets.size() << std::endl;
-    std::cout << "FREQ ITEM SETS:\n";
-    for(dftable& i: freq_itemsets)  i.show();
-#endif
-    for(size_t i = 1; i < freq_itemsets.size(); ++i) {
-       //freq-itemset 1 will not be considered
-       auto n = freq_itemsets[i].num_col();
-       for(size_t j = 0; j < n - 1; j++) {
-         auto res =  create_antacedent(freq_itemsets[i], j);
-         auto res_con = calculate_confidence(freq_itemsets[i-1],res,con);
-#ifdef FP_DEBUG
-         std::cout << "--- res_con: " << std::endl; res_con.show();
-#endif
-         if(res_con.num_row()) ass_rule.push_back(res_con);
-       }
+  generate_association_rules(std::vector<dftable>& item,
+                             std::vector<dftable>& tree_info,
+                             double conf) {
+    require(conf >= 0.0 && conf <= 1.0, 
+    "generate_rules: confidence should be in between 0 to 1.\n");
+    std::vector<dftable> rules;
+    auto depth = item.size();
+    if (depth <= 1) return rules;
+    try {
+      dftable old_info;
+      auto pre_fis = decompress_impl(item[0], tree_info[0], old_info);
+      for(size_t i = 1; i < depth; ++i) {
+        auto post_fis = decompress_impl(item[i], tree_info[i], old_info);
+        calculate_confidence(pre_fis, post_fis, conf, i, rules);
+        pre_fis = std::move(post_fis);
+      }
     }
-    return association_rule(ass_rule);
+    catch(std::exception& excpt) {
+      std::string msg = excpt.what();
+      if(msg.find("bad_alloc") != std::string::npos ) {
+        std::string e = "out-of-memory error occured during rule mining!\n";
+        e += "retry building tree with higher min_suuport value.\n";
+        REPORT_ERROR(INTERNAL_ERROR, e);
+      }
+      else REPORT_ERROR(INTERNAL_ERROR, msg);
+    }
+    return association_rule(rules);
+  }
+
+  association_rule fp_growth_model::generate_rules(double conf) {
+    return generate_association_rules(item, tree_info, conf);
+  }
+ 
+  void association_rule::clear () {
+    for(size_t i = 0; i < rule.size(); ++i) free_df(rule[i]); 
+    rule.clear();
+  }
+
+  size_t association_rule::get_depth () { return rule.size(); }
+
+  size_t association_rule::get_count() {
+    size_t count = 0;
+    for(size_t i = 0; i < rule.size(); ++i) count += rule[i].num_row();
+    return count;
   }
 
   void association_rule::debug_print() {
-    for(auto& r: rule) r.show();
+    for(size_t i = 0; i < rule.size(); ++i) {
+      std::cout << "--- rule[" << i << "] ---\n";
+      rule[i].show(); 
+      std::cout << std::endl;
+    }
   }
   
-  void association_rule::save (const std::string& fname) {
-    std::cout << "save request for fp-growth model with fname: " << fname << std::endl;
+  void association_rule::save (const std::string& dir) {
+    require(!directory_exists(dir),
+    "save: a directory with same name already exists!\n");
+    make_directory(dir);
+    size_t precision = 6;
+    std::string datetime_fmt = "%Y-%m-%d";
+    std::string sep = " ";
+    for (size_t i = 0; i < rule.size(); ++i) {
+      auto part_dname = dir + "/rule_" + std::to_string(i);
+      make_directory(part_dname);
+      auto rule_data = part_dname + "/data";
+      auto rule_schema = part_dname + "/schema";
+      auto dt = rule[i].savetext(rule_data, precision, datetime_fmt, sep);
+
+      std::ofstream schema_str;
+      schema_str.exceptions(std::ofstream::failbit | std::ofstream::badbit);
+      schema_str.open(rule_schema.c_str());
+      for(auto& e: dt) schema_str << e.first << "\n" << e.second << "\n";
+    }
   }
   
-  void association_rule::savebinary (const std::string& fname) {
-    std::cout << "savebinary request for fp-growth model with fname: " << fname << std::endl;
+  void association_rule::savebinary (const std::string& dir) {
+    require(!directory_exists(dir),
+    "save: a directory with same name already exists!\n");
+    make_directory(dir);
+    for (size_t i = 0; i < rule.size(); ++i) {
+      auto part_dname = dir + "/rule_" + std::to_string(i);
+      rule[i].save(part_dname);
+    }
   }
   
-  void association_rule::load (const std::string& fname) {
-    std::cout << "load request for fp-growth model with fname: " << fname << std::endl;
+  void association_rule::load (const std::string& dir) {
+    require(directory_exists(dir), "load: directory does not exist!\n");
+    auto depth = count_non_hidden_files(dir);
+    RLOG(INFO) << "load: set of rules found: " << depth << std::endl;
+    clear();  this->rule.resize(depth);
+    std::string tmp;
+    for(size_t i = 0; i < depth; ++i) {
+      auto rule_data = dir + "/rule_" + std::to_string(i) + "/data";
+      auto rule_schema = dir + "/rule_" + std::to_string(i) + "/schema";
+      std::ifstream schema_str(rule_schema.c_str());
+      std::vector<std::string> types, names;
+      while(std::getline(schema_str, tmp)) {
+        names.push_back(tmp);
+        std::getline(schema_str, tmp); types.push_back(tmp);
+      }
+      //show("names: ", names);
+      //show("types: ", types);
+      rule[i] = make_dftable_loadtext(rule_data, types, names, ' ');
+    }
   }
   
-  void association_rule::loadbinary (const std::string& fname) {
-    std::cout << "loadbinary request for fp-growth model with fname: " << fname << std::endl;
+  void association_rule::loadbinary (const std::string& dir) {
+    require(directory_exists(dir), "load: directory does not exist!\n");
+    auto depth = count_non_hidden_files(dir);
+    RLOG(INFO) << "load: set of rules found: " << depth << std::endl;
+    clear();  this->rule.resize(depth);
+    for(size_t i = 0; i < depth; ++i) {
+      auto rule_dir = dir + "/rule_" + std::to_string(i);
+      rule[i] = dftable();
+      rule[i].load(rule_dir);
+    }
   }
-  
-  association_rule fp_growth_model::generate_rules(double con){
-    return generate_association_rules(item,con);
-  }
-  
+
 }
