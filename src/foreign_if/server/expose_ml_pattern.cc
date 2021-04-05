18d5f81d582bfc7c51b7e2e9260ebaeda7461a1b
Takuya Araki <takuy_araki@nec.com>
2021-04-05 17:42:05 +0900

    added frovedis_fp_fis for fp-growth model conversion in server side, Frovedis to Spark association rule output conversion changes in server (NTI)

--- src/foreign_if/server/expose_ml_pattern.cc
+++ src/foreign_if/server/expose_ml_pattern.cc
@@ -8,4 +8,6 @@ void expose_frovedis_pattern_mining_functions() {
   expose(frovedis_fpr<fp_growth_model>);
   //expose(get_frovedis_fpm<std::string>);
   expose(get_frovedis_fpm<int>);
+  expose(frovedis_fp_fis<fp_growth_model>);
+  expose(frovedis_fp_rules<association_rule>);
 }
