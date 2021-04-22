4cad14ff05ebeeb9c0f21eb77248f572ece3132c
Takuya Araki <takuy_araki@nec.com>
2021-04-22 15:38:29 +0900

    correct to add get() after exrpc_async

--- src/foreign_if/python/main/cpp/ml.cc
+++ src/foreign_if/python/main/cpp/ml.cc
@@ -1821,26 +1821,26 @@ extern "C" {
           if (itype == INT)
             //exrpc_async(fm_node,(frovedis_lda_train<DT4,S_MAT44>), f_dptr, alpha,
             //             beta, num_topics, num_iter, algo, num_explore_iter, num_eval_cycle,
-            //             verbose, mid);
+            //             verbose, mid).get();
             REPORT_ERROR(USER_ERROR,
                          "Currently frovedis LDA doesn't support int itype for sparse data!\n");
           else if (itype == LONG )
             exrpc_async(fm_node,(frovedis_lda_train<DT4,S_MAT45>), f_dptr, alpha,
-                         beta, num_topics, num_iter, algo, num_explore_iter, num_eval_cycle,
-                         verbose, mid);
+                        beta, num_topics, num_iter, algo, num_explore_iter, num_eval_cycle,
+                        verbose, mid).get();
           else REPORT_ERROR(USER_ERROR, "Unsupported itype for input sparse datain LDA train!\n");
           break;
         case LONG:
           if (itype == INT)
             //exrpc_async(fm_node,(frovedis_lda_train<DT3,S_MAT34>), f_dptr, alpha,
             //             beta, num_topics, num_iter, algo, num_explore_iter, num_eval_cycle,
-            //             verbose, mid);
+            //             verbose, mid).get();
             REPORT_ERROR(USER_ERROR,
                          "Currently frovedis LDA doesn't support int itype for sparse data!\n");
           else if (itype == LONG)
             exrpc_async(fm_node,(frovedis_lda_train<DT3,S_MAT35>), f_dptr, alpha,
-                         beta, num_topics, num_iter, algo, num_explore_iter, num_eval_cycle,
-                         verbose, mid);
+                        beta, num_topics, num_iter, algo, num_explore_iter, num_eval_cycle,
+                        verbose, mid).get();
           else REPORT_ERROR(USER_ERROR, "Unsupported itype for input sparse datain LDA train!\n");
           break;
         default: REPORT_ERROR(USER_ERROR,"Unsupported dtype for input matrix!\n");
