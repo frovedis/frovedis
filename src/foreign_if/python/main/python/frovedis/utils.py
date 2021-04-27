dbee39d1772b8f570d065c79d65d61d54b13b9b6
Takuya Araki <takuy_araki@nec.com>
2021-04-27 17:44:33 +0900

    added server association checks in linear models; fixed some minor bugs (NTI)

--- src/foreign_if/python/main/python/frovedis/utils.py
+++ src/foreign_if/python/main/python/frovedis/utils.py
@@ -1,18 +1,26 @@
+"""
+utils.py: module containing utils like deprecated, 
+          is_notebook, check_sample_weight
+"""
 import warnings
+import numbers
+import numpy as np
 
 def deprecated(message):
   def deprecated_decorator(func):
       def deprecated_func(*args, **kwargs):
           warnings.simplefilter('always', DeprecationWarning)
-          warnings.warn("{}() is a deprecated function. {}".format(func.__name__, message),
-                        category=DeprecationWarning,
-                        stacklevel=2)
+          warnings.warn(\
+            "{}() is a deprecated function. {}".format(func.__name__, message),
+            category=DeprecationWarning,
+            stacklevel=2)
           warnings.simplefilter('default', DeprecationWarning)
           return func(*args, **kwargs)
       return deprecated_func
   return deprecated_decorator
 
 def is_notebook():
+    """checks whether execution environment is an ipython/jupyter notebook"""
     try:
         shell = get_ipython().__class__.__name__
         if shell == 'ZMQInteractiveShell':
@@ -23,3 +31,17 @@ def is_notebook():
             return False  # Other type (?)
     except NameError:
         return False      # Probably standard Python interpreter
+
+def check_sample_weight(self, sample_weight):
+    """checks input X and y"""
+    if sample_weight is None:
+        weight = np.array([], dtype=np.float64)
+    elif isinstance(sample_weight, numbers.Number):
+        weight = np.full(self.n_samples, sample_weight, dtype=np.float64)
+    else:
+        weight = np.ravel(sample_weight)
+        if len(weight) != self.n_samples:
+            raise ValueError("sample_weight.shape == {}, expected {}!"\
+                   .format(sample_weight.shape, (self.n_samples,)))
+    return np.asarray(weight, dtype=np.float64)
+
