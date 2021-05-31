dfd7d5ff4d4885fc5209e9d2e811677f29e9725f
Takuya Araki <takuy_araki@nec.com>
2021-05-31 12:01:10 +0900

    warm_start support on wrapper side in GLM, Added sparse support on wrapper for KNN (NTI)

--- src/foreign_if/server/short_hand_model_type.hpp
+++ src/foreign_if/server/short_hand_model_type.hpp
@@ -6,22 +6,30 @@
 #include <frovedis/ml/clustering/gmm.hpp>
 #include <frovedis/ml/tree/ensemble_model.hpp>
 #include "frovedis/ml/kernel/kernel_svm.hpp"
+#include "frovedis/ml/glm/logistic_regression.hpp"
+#include "frovedis/ml/glm/ridge_regression.hpp"
+#include "frovedis/ml/glm/lasso.hpp"
+#include "frovedis/ml/glm/linear_svc.hpp"
+#include "frovedis/ml/glm/linear_svr.hpp"
+#include "frovedis/ml/glm/linear_regression.hpp"
 #include "short_hand_dtype.hpp"
 
 namespace frovedis {
   // --- All Frovedis ML MODEL Types ---
-  typedef logistic_regression_model<DT1> LRM1;
-  typedef logistic_regression_model<DT2> LRM2;
-  typedef multinomial_logistic_regression_model<DT1> MLR1;
-  typedef multinomial_logistic_regression_model<DT2> MLR2;
-  typedef linear_regression_model<DT1> LNRM1;
-  typedef linear_regression_model<DT2> LNRM2;
-  typedef svm_model<DT1> SVM1;
-  typedef svm_model<DT2> SVM2; 
+  typedef logistic_regression<DT1> LR1;
+  typedef logistic_regression<DT2> LR2;
+  typedef ridge_regression<DT1> RR1;
+  typedef ridge_regression<DT2> RR2;
+  typedef lasso_regression<DT1> LSR1;
+  typedef lasso_regression<DT2> LSR2;
+  typedef linear_regression<DT1> LNRM1;
+  typedef linear_regression<DT2> LNRM2;
+  typedef linear_svm_classifier<DT1> SVM1;
+  typedef linear_svm_classifier<DT2> SVM2; 
   typedef kernel_csvc_model<DT1> KSVC1;
   typedef kernel_csvc_model<DT2> KSVC2; 
-  typedef linear_regression_model<DT1> SVR1;
-  typedef linear_regression_model<DT2> SVR2; 
+  typedef linear_svm_regressor<DT1> SVR1;
+  typedef linear_svm_regressor<DT2> SVR2; 
   typedef matrix_factorization_model<DT1> MFM1;
   typedef matrix_factorization_model<DT2> MFM2;
   typedef KMeans<DT1> KMM1;
@@ -46,12 +54,24 @@ namespace frovedis {
   typedef dbscan<DT2> DBSCAN2;
   typedef gaussian_mixture<DT1> GMM1;
   typedef gaussian_mixture<DT2> GMM2;    
-  typedef nearest_neighbors<DT1> KNN1;
-  typedef nearest_neighbors<DT2> KNN2;
-  typedef kneighbors_classifier<DT1> KNC1;
-  typedef kneighbors_classifier<DT2> KNC2;
-  typedef kneighbors_regressor<DT1> KNR1;
-  typedef kneighbors_regressor<DT2> KNR2;
+  typedef nearest_neighbors<DT1, R_MAT1> KNNR1;
+  typedef nearest_neighbors<DT2, R_MAT2> KNNR2;
+  typedef nearest_neighbors<DT1, S_MAT14> KNNS14;
+  typedef nearest_neighbors<DT1, S_MAT15> KNNS15;
+  typedef nearest_neighbors<DT2, S_MAT24> KNNS24;
+  typedef nearest_neighbors<DT2, S_MAT25> KNNS25;
+  typedef kneighbors_classifier<DT1, R_MAT1> KNCR1;
+  typedef kneighbors_classifier<DT2, R_MAT2> KNCR2;
+  typedef kneighbors_classifier<DT1, S_MAT14> KNCS14;
+  typedef kneighbors_classifier<DT1, S_MAT15> KNCS15;
+  typedef kneighbors_classifier<DT2, S_MAT24> KNCS24;
+  typedef kneighbors_classifier<DT2, S_MAT25> KNCS25;
+  typedef kneighbors_regressor<DT1, R_MAT1> KNRR1;
+  typedef kneighbors_regressor<DT2, R_MAT2> KNRR2;
+  typedef kneighbors_regressor<DT1, S_MAT14> KNRS14;
+  typedef kneighbors_regressor<DT1, S_MAT15> KNRS15;
+  typedef kneighbors_regressor<DT2, S_MAT24> KNRS24;
+  typedef kneighbors_regressor<DT2, S_MAT25> KNRS25;
   typedef lda_model<DT4> LDA4;
   typedef lda_model<DT3> LDA3;
   typedef lda_model_wrapper<DT3> LDASP3; //for spark
