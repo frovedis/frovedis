8730aea471e3004922b8e16a898f9642999a60d9
Takuya Araki <takuy_araki@nec.com>
2021-05-31 11:54:04 +0900

    Added sparse support on wrapper for KNN (NTI)

--- src/foreign_if/server/expose_ml_neighbor.cc
+++ src/foreign_if/server/expose_ml_neighbor.cc
@@ -8,11 +8,23 @@ void expose_frovedis_neighbor_functions() {
   // (1) knn - Nearest Neighbors (NN)
   expose((frovedis_knn<DT1,R_MAT1>));
   expose((frovedis_knn<DT2,R_MAT2>)); 
+  expose((frovedis_knn<DT1,S_MAT14>));
+  expose((frovedis_knn<DT1,S_MAT25>));
+  expose((frovedis_knn<DT2,S_MAT24>)); 
+  expose((frovedis_knn<DT2,S_MAT25>)); 
   // (2) knc
   expose((frovedis_knc<DT1,R_MAT1>));
   expose((frovedis_knc<DT2,R_MAT2>)); 
+  expose((frovedis_knc<DT1,S_MAT14>));
+  expose((frovedis_knc<DT1,S_MAT15>));
+  expose((frovedis_knc<DT2,S_MAT24>)); 
+  expose((frovedis_knc<DT2,S_MAT25>)); 
   // (3) knr
   expose((frovedis_knr<DT1,R_MAT1>));
   expose((frovedis_knr<DT2,R_MAT2>)); 
+  expose((frovedis_knr<DT1,S_MAT14>));
+  expose((frovedis_knr<DT1,S_MAT15>));
+  expose((frovedis_knr<DT2,S_MAT24>)); 
+  expose((frovedis_knr<DT2,S_MAT25>)); 
 }
 
