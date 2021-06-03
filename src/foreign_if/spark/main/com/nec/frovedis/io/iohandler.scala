24ee73d874c9439b352ebc4885a9e950aa0c2e9a
Takuya Araki <takuy_araki@nec.com>
2021-06-03 14:01:21 +0900

    added saveDictionary/loadDictionary in io module of spark wrapper for easy saving/loading of dictionary information (metadata etc.) (NTI)

--- src/foreign_if/spark/main/com/nec/frovedis/io/iohandler.scala
+++ src/foreign_if/spark/main/com/nec/frovedis/io/iohandler.scala
@@ -1,6 +1,8 @@
 package com.nec.frovedis.io;
 
 import java.io.File
+import scala.reflect.ClassTag
+import org.apache.spark.SparkContext
 
 object FrovedisIO {
   def createDir(path: String): Boolean = {
@@ -11,5 +13,17 @@ object FrovedisIO {
   }
   def checkExists(path: String): Boolean = {
     return new File(path).exists
+  }
+  def saveDictionary[K,V](target: Map[K,V],
+                          path: String): Unit = {
+    val context = SparkContext.getOrCreate()
+    context.parallelize(target.toSeq, 2).saveAsObjectFile(path)
+  }
+  def loadDictionary[K: ClassTag, V: ClassTag](path: String): Map[K,V] = {
+    val context = SparkContext.getOrCreate()
+    val target = context.objectFile[(K,V)](path)
+                        .collectAsMap() // returns generic scala.collection.Map
+                        .toMap          // to make it immutable Map
+    return target
   }
 }
