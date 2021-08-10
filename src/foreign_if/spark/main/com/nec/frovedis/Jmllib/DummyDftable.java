6653270547f7592a51ecb41ff3c2b8e28c5c5de6
Takuya Araki <takuy_araki@nec.com>
2021-08-10 15:29:02 +0900

    Added DummyDftable class for FPGrowth spark wrapper (NTI)

---
+++ src/foreign_if/spark/main/com/nec/frovedis/Jmllib/DummyDftable.java
@@ -0,0 +1,14 @@
+package com.nec.frovedis.Jmllib;
+
+public class DummyDftable implements java.io.Serializable {
+  public DummyDftable() {}
+  public DummyDftable(long dfptr, String[] names, short[] types) {
+    this.dfptr = dfptr;
+    this.names = names;
+    this.types = types;
+  }
+  public long dfptr;
+  public String[] names;
+  public short[] types;
+}
+
