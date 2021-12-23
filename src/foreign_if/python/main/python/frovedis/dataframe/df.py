291a8934d511d7b393c4f72f5c4db6a9ab1a3cf1
Takuya Araki <takuy_araki@nec.com>
2021-12-23 19:07:30 +0900

    added to_pandas_series() in python dataframe wrapper

--- src/foreign_if/python/main/python/frovedis/dataframe/df.py
+++ src/foreign_if/python/main/python/frovedis/dataframe/df.py
@@ -1473,6 +1473,15 @@ class DataFrame(object):
                 res[col].replace(to_replace={"NULL": np.nan}, inplace=True)
         return res
 
+    def to_pandas_series(self):
+        """
+        returns a pandas series object from frovedis dataframe in case is_series=True
+        """
+        if not self.is_series:
+            raise TypeError("to_pandas_series: target is not a series")
+        col = self.columns[0]
+        return self.to_pandas_dataframe()[col]
+
     @deprecated("Use to_pandas_dataframe() instead!\n")
     def to_panda_dataframe(self):
         return self.to_pandas_dataframe()
