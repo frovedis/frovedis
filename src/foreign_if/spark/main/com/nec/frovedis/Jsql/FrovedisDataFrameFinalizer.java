878215f418144806bc86e4233b2486cfe008fa30
Takuya Araki <takuy_araki@nec.com>
2022-01-12 11:14:23 +0900

    removed finalize() from FrovedisDataFrame class in scala wrapper; added initial implementation of PhantomReference based auto-finalization of FrovedisDataFrames when objects are garbage collected

---
+++ src/foreign_if/spark/main/com/nec/frovedis/Jsql/FrovedisDataFrameFinalizer.java
@@ -0,0 +1,84 @@
+package com.nec.frovedis.Jsql;
+
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.PhantomReference;
+import java.util.ArrayList;
+import com.nec.frovedis.sql.FrovedisDataFrame;
+import com.nec.frovedis.Jexrpc.JNISupport;
+import com.nec.frovedis.Jexrpc.FrovedisServer;
+import java.rmi.ServerException;
+
+public class FrovedisDataFrameFinalizer 
+  extends PhantomReference<FrovedisDataFrame> implements java.io.Serializable {
+
+  static AutoCloseThread thread = new AutoCloseThread();
+  static { thread.start(); }
+
+  private long proxy;
+
+  private FrovedisDataFrameFinalizer(FrovedisDataFrame df, 
+                                    ReferenceQueue<FrovedisDataFrame> queue) { 
+    super(df, queue);
+    this.proxy = df.get();
+  }
+
+  public void release() throws ServerException {
+    if (FrovedisServer.isUP() && proxy != -1) {
+      FrovedisServer fs = FrovedisServer.getServerInstance();
+      JNISupport.releaseFrovedisDataframe(fs.master_node, proxy);
+      String info = JNISupport.checkServerException();
+      if (!info.isEmpty()) throw new ServerException(info);
+      //System.out.println("released " + proxy);
+    }
+    thread.removeObject(this);
+    //System.out.println("reference pool size: " + thread.list.size());
+  }
+
+  public static FrovedisDataFrameFinalizer 
+  addObject(FrovedisDataFrame df) {
+    return thread.addObject(df);
+  }
+    
+  public static class AutoCloseThread extends Thread {
+    private ReferenceQueue<FrovedisDataFrame> queue = new ReferenceQueue<>();
+    private ArrayList<FrovedisDataFrameFinalizer> list = new ArrayList<FrovedisDataFrameFinalizer>();
+
+    private AutoCloseThread() {
+      setDaemon(true);
+      setName("FrovedisDataFrameFinalizer");
+    }
+
+    private FrovedisDataFrameFinalizer 
+    addObject(FrovedisDataFrame df) {
+      final FrovedisDataFrameFinalizer ref = new FrovedisDataFrameFinalizer(df, queue);
+      list.add(ref);
+      return ref;
+    }
+
+    private void 
+    removeObject(FrovedisDataFrameFinalizer ref) {
+       list.remove(ref);
+    }
+
+    @Override
+    public void run() {
+      try {
+        while (true) {
+          // queue.remove(): blocking if queue is empty
+          FrovedisDataFrameFinalizer ref = (FrovedisDataFrameFinalizer) queue.remove();
+          //System.out.println(ref.proxy + " is not properly released, doing it now...");
+          //ref.release();
+          if (FrovedisServer.isUP() && ref.proxy != -1) {
+            FrovedisServer fs = FrovedisServer.getServerInstance();
+            JNISupport.releaseFrovedisDataframeNoExcept(fs.master_node, ref.proxy); // non-blocking
+            //System.out.println("released " + ref.proxy);
+          }
+          removeObject(ref);
+          //System.out.println("reference pool size: " + list.size());
+        }
+      } catch (Exception e) {
+         System.out.println(this.getName() + ": thread interrupted => " + e.getMessage());
+      }
+    }
+  }
+}
