00e60d28f255d7761d0c64e3ba25b0665ea5c32b
Takuya Araki <takuy_araki@nec.com>
2022-03-07 20:52:47 +0900

    change not to throw exception but return null string if invalid arguments are provided to make the behavior similar to SQL/Spark

--- src/frovedis/text/words.cc
+++ src/frovedis/text/words.cc
@@ -636,41 +636,46 @@ struct words_substr_helper {
   SERIALIZE(c)
 };
 
+// all substr functions returns null string if the values are invalid
+// instead of throwing exception to make the behavior similar to SQL/Spark
+// index is 0-based, while dataframe functions are 1-based
 void substr(size_t* starts, size_t* lens, size_t num_words,
             int pos, int num) {
-  if(num < 0) throw std::runtime_error("substr: num is negative");
+  if(num < 0) { // invalid
+    for(size_t i = 0; i < num_words; i++) lens[i] = 0;
+    return;
+  }
   if(pos >= 0) {
     auto fail = find_condition(lens, num_words, words_substr_helper(pos+num));
-    auto failp = fail.data();
-    /*
-    if(fail.size() != 0)
-      throw std::runtime_error("substr: pos + num is larger than length at: " +
-                               std::to_string(fail[0])
-                               + " (local index)");
-    */
     for(size_t i = 0; i < num_words; i++) {
       starts[i] += pos;
       lens[i] = num;
     }
-    if (!fail.empty()) {
-      for(size_t i = 0; i < fail.size(); i++) {
-        auto idx = failp[i];
-        starts[idx] -= pos; // reset
-        starts[idx] += (lens[idx] - 1); // at end
-        lens[idx] = 0;
-      }
+    auto failp = fail.data();
+    auto fail_size = fail.size();
+#pragma _NEC ivdep
+#pragma _NEC vovertake
+#pragma _NEC vob
+    for(size_t i = 0; i < fail_size; i++) { // invalid
+      lens[failp[i]] = 0;
     }
   } else {
-    if(pos+num > 0)
-      throw std::runtime_error("substr: num is larger than abs(pos)");
+    if(pos+num > 0) {
+      for(size_t i = 0; i < num_words; i++) lens[i] = 0;
+      return;
+    }
     auto fail = find_condition(lens, num_words, words_substr_helper(-pos));
-    if(fail.size() != 0)
-      throw std::runtime_error("substr: abs(pos) is larger than length at: " +
-                               std::to_string(fail[0])
-                               + " (local index)");
     for(size_t i = 0; i < num_words; i++) {
       starts[i] = starts[i] + lens[i] + pos;
       lens[i] = num;
+    }
+    auto failp = fail.data();
+    auto fail_size = fail.size();
+#pragma _NEC ivdep
+#pragma _NEC vovertake
+#pragma _NEC vob
+    for(size_t i = 0; i < fail_size; i++) { // invalid
+      lens[failp[i]] = 0;
     }
   }
 }
@@ -699,19 +704,13 @@ struct words_substr_helper_posv2 {
 
 void substr(size_t* starts, size_t* lens, size_t num_words,
             const int* pos, int num) {
-  if(num < 0) throw std::runtime_error("substr: num is negative");
+  if(num < 0) {
+    for(size_t i = 0; i < num_words; i++) lens[i] = 0;
+    return;
+  }
   auto fail = find_condition_pair(lens, pos, num_words,
                                   words_substr_helper_posv1(num));
-  if(fail.size() != 0)
-    throw std::runtime_error
-      ("substr: pos + num or abs(pos) is larger than length at: " +
-       std::to_string(fail[0])
-       + " (local index)");
   auto fail2 = find_condition(pos, num_words, words_substr_helper_posv2(num));
-  if(fail2.size() != 0)
-    throw std::runtime_error("substr: num is larger than abs(pos) at: " +
-                             std::to_string(fail2[0])
-                             + " (local index)");
   for(size_t i = 0; i < num_words; i++) {
     if(pos[i] >= 0) {
       starts[i] += pos[i];
@@ -720,6 +719,22 @@ void substr(size_t* starts, size_t* lens, size_t num_words,
       starts[i] = starts[i] + lens[i] + pos[i];
       lens[i] = num;
     }
+  }
+  auto failp = fail.data();
+  auto fail_size = fail.size();
+#pragma _NEC ivdep
+#pragma _NEC vovertake
+#pragma _NEC vob
+  for(size_t i = 0; i < fail_size; i++) {
+    lens[failp[i]] = 0;
+  }
+  auto fail2p = fail2.data();
+  auto fail2_size = fail2.size();
+#pragma _NEC ivdep
+#pragma _NEC vovertake
+#pragma _NEC vob
+  for(size_t i = 0; i < fail2_size; i++) {
+    lens[fail2p[i]] = 0;
   }
 }
 
@@ -744,36 +759,58 @@ struct words_substr_helper_numv2 {
 
 void substr(size_t* starts, size_t* lens, size_t num_words,
             int pos, const int* num) {
-  auto fail = find_condition(num, num_words, is_lt<int>(0));
-  if(fail.size() != 0) 
-    throw std::runtime_error("substr: num is negative at:" +
-                             std::to_string(fail[0])
-                             + " (local index)");
+  auto failnum = find_condition(num, num_words, is_lt<int>(0));
+  auto failnump = failnum.data();
+  auto failnum_size = failnum.size();
   if(pos >= 0) {
     auto fail = find_condition_pair(lens, num, num_words,
                                     words_substr_helper_numv1(pos));
-    if(fail.size() != 0)
-      throw std::runtime_error("substr: pos + num is larger than length at: " +
-                               std::to_string(fail[0])
-                               + " (local index)");
     for(size_t i = 0; i < num_words; i++) {
       starts[i] += pos;
       lens[i] = num[i];
     }
+    auto failp = fail.data();
+    auto fail_size = fail.size();
+#pragma _NEC ivdep
+#pragma _NEC vovertake
+#pragma _NEC vob
+    for(size_t i = 0; i < fail_size; i++) {
+      lens[failp[i]] = 0;
+    }
+#pragma _NEC ivdep
+#pragma _NEC vovertake
+#pragma _NEC vob
+    for(size_t i = 0; i < failnum_size; i++) {
+      lens[failnump[i]] = 0;
+    }
   } else {
     auto fail = find_condition(num, num_words, words_substr_helper_numv2(pos));
-    if(fail.size() != 0)
-      throw std::runtime_error("substr: num is larger than abs(pos) at: " +
-                               std::to_string(fail[0])
-                               + " (local index)");
     auto fail2 = find_condition(lens, num_words, words_substr_helper(-pos));
-    if(fail2.size() != 0)
-      throw std::runtime_error("substr: abs(pos) is larger than length at: " +
-                               std::to_string(fail2[0])
-                               + " (local index)");
     for(size_t i = 0; i < num_words; i++) {
       starts[i] = starts[i] + lens[i] + pos;
       lens[i] = num[i];
+    }
+    auto failp = fail.data();
+    auto fail_size = fail.size();
+#pragma _NEC ivdep
+#pragma _NEC vovertake
+#pragma _NEC vob
+    for(size_t i = 0; i < fail_size; i++) {
+      lens[failp[i]] = 0;
+    }
+    auto fail2p = fail2.data();
+    auto fail2_size = fail2.size();
+#pragma _NEC ivdep
+#pragma _NEC vovertake
+#pragma _NEC vob
+    for(size_t i = 0; i < fail2_size; i++) {
+      lens[fail2p[i]] = 0;
+    }
+#pragma _NEC ivdep
+#pragma _NEC vovertake
+#pragma _NEC vob
+    for(size_t i = 0; i < failnum_size; i++) {
+      lens[failnump[i]] = 0;
     }
   }
 }
@@ -806,33 +843,17 @@ struct words_substr_helper_posnumv3 {
 void substr(size_t* starts, size_t* lens, size_t num_words,
             const int* pos, const int* num) {
   auto fail = find_condition(num, num_words, is_lt<int>(0));
-  if(fail.size() != 0) 
-    throw std::runtime_error("substr: num is negative at:" +
-                             std::to_string(fail[0])
-                             + " (local index)");
   std::vector<int> posnum(num_words);
   auto posnump = posnum.data();
   for(size_t i = 0; i < num_words; i++) posnump[i] = pos[i] + num[i];
   // if pos is negative, this should be OK
   auto fail2 = find_condition_pair(lens, posnump, num_words,
                                    words_substr_helper_posnumv1());
-  if(fail2.size() != 0)
-    throw std::runtime_error("substr: pos + num is larger than length at: " +
-                             std::to_string(fail2[0])
-                             + " (local index)");
   // if pos is positive, this should be OK
   auto fail3 = find_condition_pair(lens, pos, num_words,
                                    words_substr_helper_posnumv2());
-  if(fail3.size() != 0)
-    throw std::runtime_error("substr: abs(pos) is larger than length at: " +
-                             std::to_string(fail3[0])
-                             + " (local index)");
   auto fail4 = find_condition_pair(pos, num, num_words,
                                    words_substr_helper_posnumv3());
-  if(fail4.size() != 0)
-    throw std::runtime_error("substr: num is larger than abs(pos) at: " +
-                             std::to_string(fail4[0])
-                             + " (local index)");
   for(size_t i = 0; i < num_words; i++) {
     if(pos[i] >= 0) {
       starts[i] += pos[i];
@@ -842,29 +863,63 @@ void substr(size_t* starts, size_t* lens, size_t num_words,
       lens[i] = num[i];
     }
   }
+  auto failp = fail.data();
+  auto fail_size = fail.size();
+#pragma _NEC ivdep
+#pragma _NEC vovertake
+#pragma _NEC vob
+  for(size_t i = 0; i < fail_size; i++) {
+    lens[failp[i]] = 0;
+  }
+  auto fail2p = fail2.data();
+  auto fail2_size = fail2.size();
+#pragma _NEC ivdep
+#pragma _NEC vovertake
+#pragma _NEC vob
+  for(size_t i = 0; i < fail2_size; i++) {
+    lens[fail2p[i]] = 0;
+  }
+  auto fail3p = fail3.data();
+  auto fail3_size = fail3.size();
+#pragma _NEC ivdep
+#pragma _NEC vovertake
+#pragma _NEC vob
+  for(size_t i = 0; i < fail3_size; i++) {
+    lens[fail3p[i]] = 0;
+  }
+  auto fail4p = fail4.data();
+  auto fail4_size = fail4.size();
+#pragma _NEC ivdep
+#pragma _NEC vovertake
+#pragma _NEC vob
+  for(size_t i = 0; i < fail4_size; i++) {
+    lens[fail4p[i]] = 0;
+  }
 }
 
 void substr(size_t* starts, size_t* lens, size_t num_words,
             int pos) {
   if(pos >= 0) {
     auto fail = find_condition(lens, num_words, words_substr_helper(pos));
-    if(fail.size() != 0)
-      throw std::runtime_error("substr: pos is larger than length at: " +
-                               std::to_string(fail[0])
-                               + " (local index)");
     for(size_t i = 0; i < num_words; i++) {
       starts[i] += pos;
       lens[i] -= pos;
     }
+    auto failp = fail.data();
+    auto fail_size = fail.size();
+    for(size_t i = 0; i < fail_size; i++) { // invalid
+      lens[failp[i]] = 0;
+    }
   } else {
     auto fail = find_condition(lens, num_words, words_substr_helper(-pos));
-    if(fail.size() != 0)
-      throw std::runtime_error("substr: abs(pos) is larger than length at: " +
-                               std::to_string(fail[0])
-                               + " (local index)");
     for(size_t i = 0; i < num_words; i++) {
       starts[i] = starts[i] + lens[i] + pos;
       lens[i] = -pos;
+    }
+    auto failp = fail.data();
+    auto fail_size = fail.size();
+    for(size_t i = 0; i < fail_size; i++) { // invalid
+      lens[failp[i]] = 0;
     }
   }
 }
@@ -883,11 +938,6 @@ void substr(size_t* starts, size_t* lens, size_t num_words,
             const int* pos) {
   auto fail = find_condition_pair(lens, pos, num_words,
                                   words_substr_helper_posvonly());
-  if(fail.size() != 0)
-    throw std::runtime_error
-      ("substr: pos or abs(pos) is larger than length at: " +
-       std::to_string(fail[0])
-       + " (local index)");
   for(size_t i = 0; i < num_words; i++) {
     if(pos[i] >= 0) {
       starts[i] += pos[i];
@@ -897,6 +947,15 @@ void substr(size_t* starts, size_t* lens, size_t num_words,
       lens[i] = -pos[i];
     }
   }
+  auto failp = fail.data();
+  auto fail_size = fail.size();
+#pragma _NEC ivdep
+#pragma _NEC vovertake
+#pragma _NEC vob
+  for(size_t i = 0; i < fail_size; i++) {
+    lens[failp[i]] = 0;
+  }
+
 }
 
 void substr(std::vector<size_t>& starts,
